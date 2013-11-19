ab00b85a53a4c078e895e17af01c912103ed6356
aquint <aquint@quarkslab.com>
2013-11-19 17:37:38 +0100

    WIP

--- braintasks.py
+++ braintasks.py
@@ -24,8 +24,8 @@ def ping():
 @celery.task(serializer='pickle')
 def scan(oid):
    # create one subtask per oid to scan
-   job = sondetasks.sonde_scan.s(oid)
-   return job.apply_async()
+   job = sondetasks.sonde_scan.delay(oid)
+   return job
 
 @celery.task(serializer='pickle')
 def scanarchive(oid):
