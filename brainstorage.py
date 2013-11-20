b3ca732d1a04bfd7d931e09e9c9ff46df1b3a847
aquint <aquint@quarkslab.com>
2013-11-20 11:09:43 +0100

    WIP

--- brainstorage.py
+++ brainstorage.py
@@ -5,13 +5,6 @@ from pymongo import MongoClient
 
         
 class BrainStorage(object):
-   _instance = None
-   def __new__(cls, *args, **kwargs):
-      if not cls._instance:
-         cls._instance = super(BrainStorage, cls).__new__(
-                          cls, *args, **kwargs)
-      return cls._instance
-        
    def __init__(self):
       self.dbh = None
       return
