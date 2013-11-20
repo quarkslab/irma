7c5625da1902724c634402324f841ac63ed713e7
aquint <aquint@quarkslab.com>
2013-11-20 11:02:58 +0100

    WIP

--- brainstorage.py
+++ brainstorage.py
@@ -8,7 +8,7 @@ class BrainStorage(object):
    _instance = None
    def __new__(cls, *args, **kwargs):
       if not cls._instance:
-         cls._instance = super(Singleton, cls).__new__(
+         cls._instance = super(BrainStorage, cls).__new__(
                           cls, *args, **kwargs)
       return cls._instance
         
