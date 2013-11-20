5e1a0ab211eff7b071f6bd106d298f24484e1d70
aquint <aquint@quarkslab.com>
2013-11-20 10:54:04 +0100

    WIP

--- brainstorage.py
+++ brainstorage.py
@@ -2,19 +2,22 @@ import gridfs
 import uuid
 from bson import ObjectId
 from pymongo import MongoClient
-     
+
+dbh = None
 def __dbconn():
    client = MongoClient('mongodb://192.168.130.133:27017/')
    db = client.irma_test_database
    return gridfs.GridFS(db)  
       
 def store_file(data):
-   dbh = __dbconn()
+   if not dbh:
+      dbh = __dbconn()
    oid = str(dbh.put(data))
    return oid
    
 def get_file(oid):
-   dbh = __dbconn()
+   if not dbh:
+      dbh = __dbconn()
    return dbh.get(ObjectId(oid)).read()
       
 
