925122c11e6075fa7fa582b32cfa0273bd0fb627
aquint <aquint@quarkslab.com>
2013-11-23 15:37:11 +0100

    initial commit

---
+++ brainstorage.py
@@ -0,0 +1,29 @@
+import gridfs
+import uuid
+from bson import ObjectId
+from pymongo import MongoClient
+
+        
+class BrainStorage(object):
+   def __init__(self):
+      self.dbh = None
+      return
+      
+   def __dbconn(self):
+      client = MongoClient('mongodb://192.168.130.133:27017/')
+      db = client.irma_test_database
+      self.dbh = gridfs.GridFS(db)  
+         
+   def store_file(self,data):
+      if not self.dbh:
+         self.__dbconn()
+      oid = str(self.dbh.put(data))
+      return oid
+      
+   def get_file(self,oid):
+      if not self.dbh:
+         self.__dbconn()
+      return self.dbh.get(ObjectId(oid)).read()
+      
+
+
---
+++ braintasks.py
@@ -0,0 +1,60 @@
+import time
+import libarchive
+from celery import Celery,exceptions
+from celery.result import GroupResult
+from celery.task import group
+from sonde import sondetasks
+from config.config import IRMA_TIMEOUT
+
+celery=Celery('braintasks')
+celery.config_from_object('config.brainconfig')
+
+@celery.task
+def ping():
+   res = "Brain is up and running\n"
+   try:   
+      task = sondetasks.ping.delay()        
+      while not task.ready():
+         time.sleep(1)
+      res += task.get(timeout=IRMA_TIMEOUT)
+   except exceptions.TimeoutError:
+      res += "Sonde is down"
+   return res
+
+@celery.task()
+def scan(oid):
+   # create one subtask per oid to scan
+   t1 = sondetasks.sonde_scan.apply_async(args=[oid],queue='kaspersky')
+   t2 = sondetasks.sonde_scan.apply_async(args=[oid],queue='clamav', countdown=2)
+   t3 = sondetasks.sonde_scan.apply_async(args=[oid],queue='sophos', countdown=4)
+   res = GroupResult(results=[t1,t2,t3])
+   return res.get()
+   
+@celery.task()
+def scanarchive(oid):
+   try:   
+      data = brainstorage.get_file(oid)
+      archfile = libarchive.Archive(data)
+      oidlist = []
+      for entry in archfile:
+          print "Unzip",entry.pathname
+          oid = brainstorage.store_file(archfile.read())
+          oidlist.append(oid)
+          time.sleep(1)
+      archfile.close()
+      tasklist = []
+      for oid in oidlist:
+         task = sondetasks.sonde_scan.delay(oid)
+         time.sleep(0.1)
+      while tasklist:
+         print "Tasklist contains: %d",len(tasklist)
+         t = tasklist.pop()
+         if not t.ready():
+            tasklist.append(t)
+            time.sleep(1)
+         else:
+            res += t.get(timeout=IRMA_TIMEOUT)
+   except exceptions.TimeoutError:
+      res = "Sonde is down"
+   return res
+
