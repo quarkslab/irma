950089ea74ff3bec4210de4dae5f70718b0b33d2
aquint <aquint@quarkslab.com>
2013-11-25 16:38:31 +0100

    WIP

--- braintasks.py
+++ braintasks.py
@@ -22,12 +22,11 @@ def ping():
    return res
 
 @celery.task()
-def scan(oid):
-   # create one subtask per oid to scan
-   t1 = sondetasks.sonde_scan.apply_async(args=[oid],queue='kaspersky')
-   t2 = sondetasks.sonde_scan.apply_async(args=[oid],queue='clamav', countdown=2)
-   t3 = sondetasks.sonde_scan.apply_async(args=[oid],queue='sophos', countdown=4)
-   res = GroupResult(results=[t1,t2,t3])
+def scan(oids):
+   # create one subtask per oid to scan per antivirus queue
+   for av in ['kaspersky','clamav']:
+      tasks += [sondetasks.sonde_scan.s(args=[oid],queue=av) for oid in oids]
+   res = tasks.apply_async()
    return res.get()
    
 @celery.task()
