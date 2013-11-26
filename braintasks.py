eb468061278305d464c3f71631fd1e258624f564
aquint <aquint@quarkslab.com>
2013-11-26 11:20:30 +0100

    WIP

--- braintasks.py
+++ braintasks.py
@@ -6,10 +6,10 @@ from celery.task import group
 from sonde import sondetasks
 from config.config import IRMA_TIMEOUT
 
-celery=Celery('braintasks')
-celery.config_from_object('config.brainconfig')
+celery_brain=Celery('braintasks')
+celery_brain.config_from_object('config.brainconfig')
 
-@celery.task
+@celery_brain.task(name="brain.ping")
 def ping():
    res = "Brain is up and running\n"
    try:   
@@ -21,7 +21,7 @@ def ping():
       res += "Sonde is down"
    return res
 
-@celery.task()
+@celery_brain.task(name="brain.scan")
 def scan(oids):
    # create one subtask per oid to scan per antivirus queue
    tasks = []
@@ -30,33 +30,4 @@ def scan(oids):
          print "Task av %s oid %s"%(av,oid)
          tasks.append(sondetasks.sonde_scan.subtask(args=[oid],queue=av))
    res = group(tasks).apply_async()
-   return res.get()
-   
-@celery.task()
-def scanarchive(oid):
-   try:   
-      data = brainstorage.get_file(oid)
-      archfile = libarchive.Archive(data)
-      oidlist = []
-      for entry in archfile:
-          print "Unzip",entry.pathname
-          oid = brainstorage.store_file(archfile.read())
-          oidlist.append(oid)
-          time.sleep(1)
-      archfile.close()
-      tasklist = []
-      for oid in oidlist:
-         task = sondetasks.sonde_scan.delay(oid)
-         time.sleep(0.1)
-      while tasklist:
-         print "Tasklist contains: %d",len(tasklist)
-         t = tasklist.pop()
-         if not t.ready():
-            tasklist.append(t)
-            time.sleep(1)
-         else:
-            res += t.get(timeout=IRMA_TIMEOUT)
-   except exceptions.TimeoutError:
-      res = "Sonde is down"
-   return res
-
+   return res.get()  
