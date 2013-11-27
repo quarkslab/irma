ab4e433588050ee1d8d4c76487de288261493024
aquint <aquint@quarkslab.com>
2013-11-27 12:04:13 +0100

    WIP

--- brainstorage.py
+++ brainstorage.py
@@ -1,29 +1,35 @@
 import gridfs
 import uuid
 from bson import ObjectId
-from pymongo import MongoClient
+import pymongo
+import hashlib
 
         
 class BrainStorage(object):
+   GRIDFS_DB = "irma_test_database"
+   
    def __init__(self):
       self.dbh = None
+      self.hash = hashlib.sha256
       return
       
    def __dbconn(self):
-      client = MongoClient('mongodb://192.168.130.133:27017/')
-      db = client.irma_test_database
+      client = pymongo.MongoClient('mongodb://192.168.130.133:27017/')
+      db = client.GRIDFS_DB
       self.dbh = gridfs.GridFS(db)  
+      return 
          
-   def store_file(self,data):
+   def store_file(self,data, name=None, date_lastresult=None):
+      """ put data into gridfs and get file object-id """
       if not self.dbh:
          self.__dbconn()
-      oid = str(self.dbh.put(data))
+      datahash = self.hash(data).hexdigest()
+      oid = str(self.dbh.put(data, filename=name, hexdigest=datahash, date_lastresult=date_lastresult))
       return oid
       
    def get_file(self,oid):
+      """ get data from gridfs by file object-id """
       if not self.dbh:
          self.__dbconn()
-      return self.dbh.get(ObjectId(oid)).read()
-      
-
+      return self.dbh.get(ObjectId(file_oid))
 
