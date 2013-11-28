1138cde86445c820737f24ff73f2c1fe98a5e566
aquint <aquint@quarkslab.com>
2013-11-28 11:08:45 +0100

    WIP

--- brainstorage.py
+++ brainstorage.py
@@ -7,75 +7,88 @@ import copy
 
         
 class BrainStorage(object):
-
+   RESCOLL = "res"
+   SCANCOLL = "scan"
+   
    def __init__(self):
       self.dbh = None
       self.hash = hashlib.sha256
       return
       
-   def __dbconn(self):
-      client = pymongo.MongoClient('mongodb://192.168.130.133:27017/')
-      self.dbh = client.irma_test
-      return 
-         
-   def store_file(self,data, name=None , date_lastresult=None):
-      """ put data into gridfs and get file object-id """
+   def __dbconn(self, collection_name=None):
       if not self.dbh:
-         self.__dbconn()
+         client = pymongo.MongoClient('mongodb://192.168.130.133:27017/')
+         self.dbh = client.irma_test
+      if collection_name:
+         return self.dbh[collection_name]
+      return
+      
+   def __update_record(self, collection_name, oid, update):
+      """ update scan record with update """      
+      dbh = self.__dbconn(collection_name)
+      record = dbh.find_one({'_id':ObjectId(oid)})
+      for key, value in update.items():
+         record[key] = value
+      dbh.save(record)
+      return
+     
+#______________________________________________________________ FILE API
+              
+   def store_file(self, data, name=None):
+      """ put data into gridfs and get file object-id """
+      self.__dbconn()
       fsdbh = gridfs.GridFS(self.dbh)        
       datahash = self.hash(data).hexdigest()
-      oid = fsdbh.put(data, filename=name, hexdigest=datahash, date_lastresult=date_lastresult)
-      return str(oid)
+      file_oid = fsdbh.put(data, filename=name, hexdigest=datahash)
+      return str(file_oid)
       
-   def get_file(self,oid):
+   def get_file(self,file_oid):
       """ get data from gridfs by file object-id """
-      if not self.dbh:
-         self.__dbconn()
+      self.__dbconn()
       fsdbh = gridfs.GridFS(self.dbh)        
-      return fsdbh.get(ObjectId(oid))
+      return fsdbh.get(ObjectId(file_oid))
 
+#______________________________________________________________ SCAN API
 
    def create_scan_record(self, file_oids):
-      if not self.dbh:
-         self.__dbconn()
-      dbh = self.dbh.scan
-      scan_oid = dbh.save({'oids':{oid:[] for oid in file_oids}, 'avlist':[], 'nbscan':0})
+      dbh = self.__dbconn(SCANCOLL)
+      scan_oid = dbh.save({'oids': file_oids, 'avlist':[], 'nblaunched': 0, 'nbsuccess':0 })
       return str(scan_oid)
 
-   def __update_scan(self,scan_oid, update):
-      """ update scan record with update """
-      if not self.dbh:
-         self.__dbconn()
-      dbh = self.dbh.scan
-      scan = dbh.find_one({'_id':ObjectId(scan_oid)})
-      for key, value in update.items():
-         scan[key] = value
-      dbh.save(scan)
+   def update_scan_record(self, scan_oid, avlist, nbscan):      
+      self.__update_record(SCANCOLL, scan_oid, {'avlist':avlist, 'nblaunched':nbscan})
       return
 
-   def update_scan_record(self, scan_oid, avlist, nbscan):
-      print "DEBUG UPDATE %s %d"%(avlist,nbscan)
-      self.__update_scan(scan_oid,{'avlist':avlist, 'nbscan':nbscan})
-      return
-
-   def get_scanid(self,scan_oid):
-      """ get list of oids associated with scanid """
-      if not self.dbh:
-         self.__dbconn()  
-      dbh = self.dbh.scan
+   def get_scan_fileoid(self,scan_oid):
+      """ get list of file oids associated with scanid """
+      dbh = self.__dbconn(SCANCOLL)
       record = dbh.find_one({"_id":ObjectId(scan_oid)})
-      return record
-
+      return record['oids']
    
-   def update_result(self,scan_oid, file_oid, result):
-      """ put result from sonde into resultdb and link with scan """
-      if not self.dbh:
-         self.__dbconn()
-      dbh = self.dbh.res
-      res_oid = str(dbh.save(copy.copy(result)))
-      dbh = self.dbh.scan
-      scan = dbh.find_one({'_id':ObjectId(scan_oid)})
-      scan['oids'][file_oid].append(res_oid)
-      dbh.save(scan)
+#______________________________________________________________ RESULTS API   
+   
+    
+   def create_result(self, file_oid, result):
+      """ put result from scan into resultdb and link with file_oid """
+      dbh = self.__dbconn(RESCOLL)
+      dbh.save({'_id':file_oid}.append(result))
       return
+
+   def update_result(self, file_oid, update):
+      """ put result from sonde into resultdb and link with  """
+      self.__update_record(RESCOLL, file_oid, update)
+      return
+      
+ 
+   def get_res_from_fileoid(self,file_oids):
+      """ get list of results associated with scanid """
+      dbh = self.__dbconn(RESCOLL)
+      res = {}
+      for file_oid in file_oids:     
+         res.append(dbh.find_one({"_id":file_oid}))
+      return record
+   
+
+      
+   
       
