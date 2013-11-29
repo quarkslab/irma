d8b88c415778dce3bfe5cb085874f4ad608434d3
aquint <aquint@quarkslab.com>
2013-11-29 20:15:40 +0100

    add bottle frontend and mongodb backend

--- braintasks.py
+++ braintasks.py
@@ -1,60 +1,23 @@
-import time
-import libarchive
-from celery import Celery,exceptions
-from celery.result import GroupResult
+import celery
 from celery.task import group
+from brain import brainstorage
 from sonde import sondetasks
 from config.config import IRMA_TIMEOUT
 
-celery=Celery('braintasks')
-celery.config_from_object('config.brainconfig')
+celery_brain= celery.Celery('braintasks')
+celery_brain.config_from_object('config.brainconfig')
+bstorage = brainstorage.BrainStorage()
 
-@celery.task
-def ping():
-   res = "Brain is up and running\n"
-   try:   
-      task = sondetasks.ping.delay()        
-      while not task.ready():
-         time.sleep(1)
-      res += task.get(timeout=IRMA_TIMEOUT)
-   except exceptions.TimeoutError:
-      res += "Sonde is down"
-   return res
-
-@celery.task()
-def scan(oid):
-   # create one subtask per oid to scan
-   t1 = sondetasks.sonde_scan.apply_async(args=[oid],queue='kaspersky')
-   t2 = sondetasks.sonde_scan.apply_async(args=[oid],queue='clamav', countdown=2)
-   t3 = sondetasks.sonde_scan.apply_async(args=[oid],queue='sophos', countdown=4)
-   res = GroupResult(results=[t1,t2,t3])
-   return res.get()
-   
-@celery.task()
-def scanarchive(oid):
-   try:   
-      data = brainstorage.get_file(oid)
-      archfile = libarchive.Archive(data)
-      oidlist = []
-      for entry in archfile:
-          print "Unzip",entry.pathname
-          oid = brainstorage.store_file(archfile.read())
-          oidlist.append(oid)
-          time.sleep(1)
-      archfile.close()
-      tasklist = []
-      for oid in oidlist:
-         task = sondetasks.sonde_scan.delay(oid)
-         time.sleep(0.1)
-      while tasklist:
-         print "Tasklist contains: %d",len(tasklist)
-         t = tasklist.pop()
-         if not t.ready():
-            tasklist.append(t)
-            time.sleep(1)
-         else:
-            res += t.get(timeout=IRMA_TIMEOUT)
-   except exceptions.TimeoutError:
-      res = "Sonde is down"
-   return res
+@celery_brain.task(name="brain.scan")
+def scan(scanid, oids):
+   # create one subtask per oid to scan per antivirus queue
+   tasks = []
+   avlist = ['kaspersky','clamav']
+   for oid in oids:
+      for av in avlist:
+         tasks.append(sondetasks.sonde_scan.s(args=[scanid,oid],queue=av))
+   job = group(tasks).apply_async()
+   job.save()
+   bstorage.update_scan_record(scanid,{'avlist':avlist})
+   return job.get()
 
