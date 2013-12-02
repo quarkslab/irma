6d7edd0cf31d655e22a7622eda8045c62cb89a34
aquint <aquint@quarkslab.com>
2013-12-02 11:50:50 +0100

    WIP

--- braintasks.py
+++ braintasks.py
@@ -8,16 +8,16 @@ celery_brain= celery.Celery('braintasks')
 celery_brain.config_from_object('config.brainconfig')
 bstorage = brainstorage.BrainStorage()
 
-@celery_brain.task(name="brain.scan")
+@celery_brain.task(name="brain.scan", ignore_result=True)
 def scan(scanid, oids):
    # create one subtask per oid to scan per antivirus queue
    tasks = []
    avlist = ['kaspersky','clamav']
    for oid in oids:
       for av in avlist:
-         tasks.append(sondetasks.sonde_scan.s(args=[scanid,oid],queue=av))
+         tasks.append(sondetasks.sonde_scan.si(args=[scanid,oid],queue=av))
    job = group(tasks).apply_async()
    job.save()
-   bstorage.update_scan_record(scanid,{'avlist':avlist})
+   bstorage.update_scan_record(scanid,{'taskid':job.id , 'avlist':avlist})
    return job.get()
 
