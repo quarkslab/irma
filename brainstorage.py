dc6c7dff108eecc3f46c93cadd4d835f6a54e5e2
aquint <aquint@quarkslab.com>
2013-12-13 11:26:55 +0100

    all config parameters moved to config.py.sample

--- brainstorage.py
+++ brainstorage.py
@@ -12,7 +12,7 @@ class BrainStorage(object):
     def __init__(self):
         self.dbh = None
         self.hash = hashlib.sha256
-    
+
     def __dbconn(self, collection_name=None):
         """ connect if needed to the database and returns a dbhandler to the collection specified """
         if not self.dbh:
@@ -21,7 +21,7 @@ class BrainStorage(object):
         if collection_name:
             return self.dbh[collection_name]
         return
-    
+
     def __create_update_record(self, collection_name, oid, update):
         """ update record with update dict"""
         dbh = self.__dbconn(collection_name)
@@ -32,11 +32,11 @@ class BrainStorage(object):
             record[key] = value
         dbh.save(record)
         return
-    
+
     # ______________________________________________________________ FILE API
-    
+
     def store_file(self, data, name=None):
-        """ put data into gridfs and get file object-id """
+        """ put data into gridfs and returns ( IsNewElt Boolean, file object-id) """
         dbh = self.__dbconn(FSFILESCOLL)
         fsdbh = gridfs.GridFS(self.dbh)
         datahash = self.hash(data).hexdigest()
@@ -44,28 +44,28 @@ class BrainStorage(object):
         fr = dbh.find_one({'hexdigest':datahash})
         if fr:
             # if yes return the existing file_oid
-            return str(fr['_id'])
+            return (False, str(fr['_id']))
         else:
             # if not create a new record
             file_oid = fsdbh.put(data, filename=name, hexdigest=datahash)
-            return str(file_oid)
-    
+            return (True, str(file_oid))
+
     def get_file(self, file_oid):
         """ get data from gridfs by file object-id """
         self.__dbconn()
         fsdbh = gridfs.GridFS(self.dbh)
         return fsdbh.get(ObjectId(file_oid))
-    
+
     def get_file_data(self, file_oid):
         """ get data from gridfs by file object-id """
         return self.get_file(file_oid).read()
-    
+
     # ______________________________________________________________ SCAN API
-    
+
     def update_scan_record(self, scan_oid, update):
         self.__create_update_record(dbconfig.COLL_SCANINFO, scan_oid, update)
         return
-    
+
     def __get_scan_field(self, scan_oid, field):
         """ get field value for scanid """
         dbh = self.__dbconn(dbconfig.COLL_SCANINFO)
@@ -73,7 +73,7 @@ class BrainStorage(object):
         if field in scan:
             return scan[field]
         return None
-    
+
     def get_scan_results(self, scan_oid):
         """ get list of file oids associated with scanid """
         dbh = self.__dbconn(dbconfig.COLL_SCANINFO)
@@ -82,30 +82,30 @@ class BrainStorage(object):
         for (file_oid, filename) in record['oids'].items():
             res[filename] = self.get_result(file_oid)
         return res
-    
+
     def get_scan_fileoid(self, scan_oid):
         """ get list of file oids associated with scanid """
         return self.__get_scan_field(scan_oid, "oids")
-    
+
     def get_scan_taskid(self, scan_oid):
         """ get list of file oids associated with scanid """
         return self.__get_scan_field(scan_oid, 'taskid')
-    
+
     def get_scan_status(self, scan_oid):
         """ get list of file oids associated with scanid """
         return self.__get_scan_field(scan_oid, 'status')
     # ______________________________________________________________ RESULTS API
-    
+
     def update_result(self, file_oid, update):
         """ put result from sonde into resultdb and link with  """
         self.__create_update_record(dbconfig.COLL_RESINFO, file_oid, update)
         return
-    
+
     def get_result(self, file_oid):
         """ get list of results associated with scanid """
         dbh = self.__dbconn(dbconfig.COLL_RESINFO)
         return dbh.find_one({'_id':ObjectId(file_oid)}, {'_id': False})
-    
+
     def get_result_nb(self, file_oids, avlist):
         dbh = self.__dbconn(dbconfig.RESCOLL)
         oids = map(lambda o: ObjectId(o), file_oids)
@@ -113,6 +113,6 @@ class BrainStorage(object):
         for r  in dbh.find({'_id': {'$in': oids}}, {'_id': False}):
             res += len(filter(lambda a: a in avlist, r.keys()))
         return res
-    
-    
-    
+
+
+
--- braintasks.py
+++ braintasks.py
@@ -1,35 +1,102 @@
 import celery
-from celery.task import group
 from brain import brainstorage
-from sonde import sondetasks
-from config.dbconfig import SCAN_STATUS_LAUNCHED,SCAN_STATUS_FINISHED,SCAN_STATUS_CANCELLED
+from config.dbconfig import SCAN_STATUS_LAUNCHED, SCAN_STATUS_FINISHED, SCAN_STATUS_CANCELLED, SCAN_STATUS_INIT, SCAN_STATUS_CANCELLING
+import uuid
 
-
-celery_brain= celery.Celery('braintasks')
+celery_brain = celery.Celery('braintasks')
 celery_brain.config_from_object('config.brainconfig')
 bstorage = brainstorage.BrainStorage()
 
+sonde_celery = celery.Celery('sondetasks')
+sonde_celery.config_from_object('config.sondeconfig')
+
 @celery_brain.task(ignore_result=True)
 def scan(scanid, oids):
-    tasks = []
-    avlist = ['clamav','kaspersky']
-    for oid in oids:
+    bstorage.update_scan_record(scanid, {'status':SCAN_STATUS_INIT, 'oids': oids, 'avlist':[]})
+
+    avlist = ['clamav', 'kaspersky']
+    res = []
+    for (oid, oid_info) in oids.items():
         for av in avlist:
-            # create one subtask per oid to scan per antivirus queue
-            tasks.append(sondetasks.sonde_scan.subtask(args=(scanid,oid), options={'queue':av}))
-    job = group(tasks)
-    res = job.apply_async()
-    # keep the groupresult object for task status/cancel
-    res.save()
-    bstorage.update_scan_record(scanid,{'status':SCAN_STATUS_LAUNCHED, 'taskid':res.id , 'avlist':avlist})
+            if oid_info['new']:
+                # create one subtask per oid to scan per antivirus queue
+                res.append(sonde_celery.send_task("sonde.sondetasks.sonde_scan", args=(scanid, oid), queue=av))
+            else:
+                # TODO update new filename for known oid if different
+                # TODO check if resuls for all av is present else launch specific scan
+                print "oid %s (%s) already scanned not launching" % (oid, oid_info['name'])
+
+    if len(res) != 0:
+        # Build a result set with all job AsyncResult for progress/cancel operations
+        groupid = str(uuid.uuid4())
+        groupres = sonde_celery.GroupResult(id=groupid, results=res)
+
+        # keep the groupresult object for task status/cancel
+        groupres.save()
+        bstorage.update_scan_record(scanid, {'status':SCAN_STATUS_LAUNCHED, 'taskid':groupid , 'avlist':avlist})
+    else:
+        bstorage.update_scan_record(scanid, {'status':SCAN_STATUS_FINISHED, 'taskid':None , 'avlist':avlist})
     return
 
-@celery_brain.task(ignore_result=True)
+@celery_brain.task()
 def scan_finished(scanid):
-    bstorage.update_scan_record(scanid,{'status':SCAN_STATUS_FINISHED})
+    bstorage.update_scan_record(scanid, {'status':SCAN_STATUS_FINISHED})
     return
 
-@celery_brain.task(ignore_result=True)
+@celery_brain.task()
+def scan_progress(scanid):
+    status = bstorage.get_scan_status(scanid)
+    if status == SCAN_STATUS_INIT:
+        return {"result":"success", "info":"task not launched"}
+    elif status == SCAN_STATUS_LAUNCHED:
+        task_id = bstorage.get_scan_taskid(scanid)
+        if not task_id:
+            return {"result":"error", "info":"task_id not set"}
+        job = sonde_celery.GroupResult.restore(task_id)
+        if not job:
+            return {"result":"error", "info":"not a valid taskid"}
+        else:
+            nbcompleted = nbsuccessful = 0
+            for j in job:
+                if j.ready(): nbcompleted += 1
+                if j.successful(): nbsuccessful += 1
+            if nbsuccessful == len(job):
+                bstorage.update_scan_record(scanid, {'status':SCAN_STATUS_FINISHED})
+            return {"result": "success", "info":{"total":len(job), "finished":nbcompleted, "successful":nbsuccessful}}
+    elif status == SCAN_STATUS_FINISHED:
+        return {"result":"success", "info":"finished"}
+    elif status == SCAN_STATUS_CANCELLING:
+        return {"result":"success", "info":"cancelling"}
+    elif status == SCAN_STATUS_CANCELLED:
+        return {"result":"success", "info":"cancelled"}
+    return {"result":"error", "info":"unknown status %d" % status}
+
+
+@celery_brain.task()
 def scan_cancel(scanid):
-    bstorage.update_scan_record(scanid,{'status':SCAN_STATUS_CANCELLED})
-    return
+    bstorage.update_scan_record(scanid, {'status':SCAN_STATUS_CANCELLING})
+
+    status = bstorage.get_scan_status(scanid)
+    if status == SCAN_STATUS_INIT:
+        return {"result":"error", "info":"task not launched"}
+    elif status == SCAN_STATUS_LAUNCHED:
+        task_id = bstorage.get_scan_taskid(scanid)
+        job = sonde_celery.GroupResult.restore(task_id)
+        if not job:
+            return {"result":"error", "info":"not a valid taskid"}
+        else:
+            nbcompleted = nbcancelled = 0
+            for j in job:
+                if j.ready():
+                    nbcompleted += 1
+                else:
+                    j.revoke(terminate=True)
+                    nbcancelled += 1
+            bstorage.update_scan_record(scanid, {'status':SCAN_STATUS_CANCELLED})
+            return {"result": "success", "info": {"total":len(job), "finished":nbcompleted, "cancelled":nbcancelled}}
+    elif status == SCAN_STATUS_FINISHED:
+        return {"result":"success", "info":"finished"}
+    elif status == SCAN_STATUS_CANCELLED:
+        return {"result":"success", "info":"cancelled"}
+
+    return {"result":"error"}
