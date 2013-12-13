f6cfe346c14dd78a027b3a929468f71d5ad15c86
aquint <aquint@quarkslab.com>
2013-12-13 15:35:18 +0100

    normalize output and return code even more generic config files

--- web/frontend.py
+++ web/frontend.py
@@ -1,10 +1,9 @@
 import re
 import os
 import celery
-from celery import signature
 from bottle import route, request, default_app, run
 from brain import brainstorage
-from config.dbconfig import SCAN_STATUS_INIT, SCAN_STATUS_LAUNCHED, SCAN_STATUS_FINISHED, SCAN_STATUS_CANCELLED
+from lib.irma.common.utils import IRMA_RETCODE_OK, IRMA_RETCODE_WARNING, IRMA_RETCODE_ERROR
 from bson import ObjectId
 from config.config import IRMA_TIMEOUT
 
@@ -12,10 +11,25 @@ bstorage = brainstorage.BrainStorage()
 brain_celery = celery.Celery('braintasks')
 brain_celery.config_from_object('config.brainconfig')
 
+# ______________________________________________________________ RESPONSE FORMATTER
+
+def response(code, info):
+    return {"result":code, "info":info}
+
+def error(info):
+    return response(IRMA_RETCODE_ERROR, info)
+
+def warning(info):
+    return response(IRMA_RETCODE_WARNING, info)
+
+def success(info):
+    return response(IRMA_RETCODE_OK, info)
+
+# ______________________________________________________________ SERVER ROOT
+
 @route("/")
 def svr_index():
-    return "This is irma-brain:\n"
-
+    return success("This is irma-brain")
 # ______________________________________________________________ API SCAN
 
 def validid(scanid):
@@ -34,53 +48,58 @@ def scan_new():
         oids[file_oid] = {"name": filename, "new": new}
     scanid = str(ObjectId())
     brain_celery.send_task("brain.braintasks.scan", args=(scanid, oids))
-    return {"scanid":scanid}
+    return success({"scanid":scanid})
 
 @route("/scan/results/<scanid>", method='GET')
 def scan_results(scanid):
     # Filter malformed scanid
     if not validid(scanid):
-        return {"error": "not a valid scanid"}
-    res = bstorage.get_scan_results(scanid)
-    return {"result": res}
+        return error("not a valid scanid")
+    # Launch a synchronous task (blocking for max IRMA_TIMEOUT seconds)
+    try:
+        task = brain_celery.send_task("brain.braintasks.scan_result", args=[scanid])
+        (status, res) = task.get(timeout=IRMA_TIMEOUT)
+    except celery.exceptions.TimeoutError:
+        return error("timeout")
+    return response(status, res)
 
 @route("/scan/progress/<scanid>", method='GET')
 def scan_progress(scanid):
     # Filter malformed scanid
     if not validid(scanid):
-        return {"result":"error", "info":"not a valid scanid"}
+        return error("not a valid scanid")
     # Launch a synchronous task (blocking for max IRMA_TIMEOUT seconds)
     try:
         task = brain_celery.send_task("brain.braintasks.scan_progress", args=[scanid])
-        res = task.get(timeout=IRMA_TIMEOUT)
+        (status, res) = task.get(timeout=IRMA_TIMEOUT)
     except celery.exceptions.TimeoutError:
-        return {"result":"error", "info":"timeout"}
-    return res
+        return error("timeout")
+    return response(status, res)
 
 
 @route("/scan/cancel/<scanid>", method='GET')
 def scan_cancel(scanid):
     # Filter malformed scanid
     if not validid(scanid):
-        return {"result":"error", "info":"not a valid scanid"}
+        return error("not a valid scanid")
     # Launch a synchronous task (blocking for max IRMA_TIMEOUT seconds)
     try:
         task = brain_celery.send_task("brain.braintasks.scan_cancel", args=(scanid))
-        res = task.get(timeout=IRMA_TIMEOUT)
+        (status, res) = task.get(timeout=IRMA_TIMEOUT)
     except celery.exceptions.TimeoutError:
-        return {"result":"error", "info":"timeout"}
-    return res
+        return error("timeout")
+    return response(status, res)
 # ______________________________________________________________ API STATUS
 
 @route("/status")
 def status():
-    return {"result": "TODO"}
+    return error("TODO")
 
 # ______________________________________________________________ API EXPORT
 
 def export(filename, oid):
     """ retrieve a file previously sent to the brain """
-    return {"result": "TODO"}
+    return error("TODO")
 
 # ______________________________________________________________ MAIN
 
