52c553939dbe8c8cd6e68902536c6234140e94fe
aquint <aquint@quarkslab.com>
2014-01-14 11:46:54 +0100

    rename sonde -> probe

--- irma/common/utils.py
+++ irma/common/utils.py
@@ -1,3 +1,20 @@
-IRMA_RETCODE_OK = "success"
-IRMA_RETCODE_WARNING = "warning"
-IRMA_RETCODE_ERROR = "error"
+# ______________________________________________________________ RESPONSE FORMATTER
+
+def response(code, info):
+    return {"result":code, "info":info}
+
+def success(info):
+    return (IrmaRetCode.success, info)
+
+def warning(info):
+    return (IrmaRetCode.warning, info)
+
+def error(info):
+    return (IrmaRetCode.error, info)
+
+# ______________________________________________________________ RETURN CODE
+
+class IrmaRetCode:
+    success = "success"
+    warning = "warning"
+    error = "error"
