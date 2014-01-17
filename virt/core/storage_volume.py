ee9eabcc133d78b38bb15715ece7595721caeaad
Fernand Lone-Sang <flonesang@quarkslab.com>
2014-01-17 14:49:58 +0100

    wip: storage volume manager

--- virt/core/storage_volume.py
+++ virt/core/storage_volume.py
@@ -1,11 +1,12 @@
-import logging, libvirt, xmltodict
+import logging, libvirt
 
 from lib.common import compat
 from lib.common.utils import UUID
 from lib.common.oopatterns import ParametricSingleton
 from lib.virt.core.connection import ConnectionManager
 from lib.virt.core.storage_pool import StoragePoolManager
-from lib.virt.core.exceptions import StoragePoolManagerError, StorageVolumeManagerError
+from lib.virt.core.mapper.storage_volume import StorageVolume
+from lib.virt.core.exceptions import StoragePoolManagerError, StorageVolumeManagerError, StorageVolumeError
 
 log = logging.getLogger(__name__)
 
@@ -46,8 +47,6 @@ class StorageVolumeManager(ParametricSingleton):
     ACTIVE = 1
     INACTIVE = 2
 
-    # Available status
-
     ##########################################################################
     # constructor and destructor stuff
     ##########################################################################
@@ -57,7 +56,7 @@ class StorageVolumeManager(ParametricSingleton):
 
         :param connection: either an instance of a ``ConnectionManager`` or directly a libvirt connection handler
         :param prefetch: set to True if prefetching storage pool handlers for this connection is required
-        :raises: StoragePoolManagerError if ``connection`` is not an expected type or None
+        :raises: StorageVolumeManagerError if ``connection`` is not an expected type or None
         """
         if not connection:
             raise StorageVolumeManagerError("'connection' field value '{0}' is not valid".format(connection))
@@ -76,11 +75,10 @@ class StorageVolumeManager(ParametricSingleton):
         
         self._pool = pool
 
-        # TODO: implement prefetch
-##         if prefetch:
-##             pools = self.list()
-##             for pool in pools:
-##                 self.lookup(pool)
+        if prefetch:
+            vols = self.list()
+            for vol in vols:
+                self.lookup(vol)
 
     ##########################################################################
     # context manager stuff
@@ -104,7 +102,7 @@ class StorageVolumeManager(ParametricSingleton):
     def _lookupByName(self, name):
         # type checking
         if not isinstance(name, basestring):
-            raise StoragePoolManagerError("'name' field type '{0}' is not valid".format(type(name)))
+            raise StorageVolumeManagerError("'name' field type '{0}' is not valid".format(type(name)))
 
         handle = None
         # check if storage pool has already been cached
@@ -124,8 +122,8 @@ class StorageVolumeManager(ParametricSingleton):
 
     def _lookupByKey(self, key):
         # type checking
-        if not isinstance(key, basestring) or not UUID.validate(key):
-            raise StoragePoolManagerError("'key' field '{0}' is not valid".format(key))
+        if not isinstance(key, basestring):
+            raise StorageVolumeManagerError("'key' field '{0}' is not valid".format(key))
 
         handle = None
         # check if domain has already been cached
@@ -145,7 +143,7 @@ class StorageVolumeManager(ParametricSingleton):
     def _lookupByPath(self, path):
         # type checking
         if not isinstance(path, basestring):
-            raise StoragePoolManagerError("'path' field '{0}' is not valid".format(path))
+            raise StorageVolumeManagerError("'path' field '{0}' is not valid".format(path))
 
         handle = None
         # check if domain has already been cached
@@ -168,6 +166,9 @@ class StorageVolumeManager(ParametricSingleton):
 
     def lookup(self, volume):
         handle = None
+        # force refresh
+        # extra flags; not used yet, so callers should always pass 0
+        self._pool.refresh(flags=0)
         if isinstance(volume, (tuple, list)):
             handle = list()
             for onevolume in volume:
@@ -180,7 +181,7 @@ class StorageVolumeManager(ParametricSingleton):
             if not handle:
                 handle = self._lookupByPath(volume)
             if not handle:
-                log.warn("Unable to find volume {0} in {1} on {2}", volume, self._pool.name(), self._uri)
+                log.warn("Unable to find volume {0} in {1} on {2}", volume, self._pool.name(), self._drv.getURI())
         return handle
 
     def list(self):
@@ -197,36 +198,91 @@ class StorageVolumeManager(ParametricSingleton):
 
         return tuple(labels)
 
-    # TODO: add more input types
-    # TODO: test clone with a different remote pool
+    def create(self, volume, alloc_metadata=0):
+        if not volume or not isinstance(volume, StorageVolume):
+            raise StorageVolumeManagerError("'volume' field '{0}' is not valid".format(volume))
+
+        # value checking
+        vol = self.lookup(volume.name)
+        if vol:
+            raise StorageVolumeManagerError("'{0}' already exists at {1}, abort.".format(volume.name, vol.path()))
+        try:
+            vol_xml = volume.unparse()
+            self._pool.createXML(vol_xml, flags=alloc_metadata)
+        except StorageVolumeError as e:
+            raise StorageVolumeManagerError("{0}".format(e))
+        except libvirt.libvirtError as e:
+            raise StorageVolumeManagerError("{0}".format(e))
+
     def clone(self, src, dest):
         if not isinstance(src, basestring):
-            raise StoragePoolManagerError("'src' field '{0}' is not valid".format(path))
+            raise StorageVolumeManagerError("'src' field '{0}' is not valid".format(src))
         if not isinstance(dest, basestring):
-            raise StoragePoolManagerError("'dest' field '{0}' is not valid".format(path))
+            raise StorageVolumeManagerError("'dest' field '{0}' is not valid".format(dest))
 
         orig_vol = self.lookup(src)
         if not orig_vol:
-            raise StoragePoolManagerError("Unable to find volume '{0}' on '{1}'".format(src, self._pool.name()))
+            raise StorageVolumeManagerError("Unable to find volume '{0}' on '{1}'".format(src, self._pool.name()))
     
         try:
             # extra flags; not used yet, so callers should always pass 0
             orig_xml = orig_vol.XMLDesc(0)
-            orig_dict = xmltodict.parse(orig_xml)
-        except libvirt.libvirtError as e:
-            raise StoragePoolManagerError("Unable to find volume '{0}' on '{1}'".format(src, self._pool.name()))
-
-        # create new volume
-        try:
-            new_dict = orig_dict
-            del new_dict['volume']['key']
-            new_dict['volume']['name'] = dest
-            new_xml = xmltodict.unparse(new_dict)
+            orig_obj = StorageVolume.parse(orig_xml)
+            new_obj = orig_obj
+            new_obj.key = None
+            new_obj.name = dest
+            new_xml = new_obj.unparse()
             new_vol = self._pool.createXMLFrom(new_xml, orig_vol, 0)
-        except KeyError as e:
-            raise StoragePoolManagerError("Malformed XML, abort ...")
+        except StorageVolumeError as e:
+            raise StorageVolumeManagerError("{0}".format(e))
         except libvirt.libvirtError as e:
-            raise StoragePoolManagerError("Unable to create volume '{0}' on '{1}'".format(dest, self._pool.name()))
+            raise StorageVolumeManagerError("Unable to find volume '{0}' on '{1}'".format(src, self._pool.name()))
 
         # update cache
         return self.lookup(dest)
+
+    # TODO: refactoring
+    def download(self, volume, filename, offset=0, length=0, flags=0):
+        if not isinstance(volume, basestring):
+            raise StorageVolumeManagerError("'volume' field '{0}' is not valid".format(volume))
+
+        name = volume
+        volume = self.lookup(volume)
+        if not volume:
+            raise StorageVolumeManagerError("Unable to find volume '{0}'".format(name))
+
+        try:
+            file = open(filename, 'wb')
+            stream = self._drv.newStream(flags=0)
+            # extra flags; not used yet, so callers should always pass 0
+            mime = volume.download(stream, offset, length, flags=0)
+            stream.recvAll(lambda stream, data, fdes: fdes.write(data), file)
+            stream.finish()
+            file.close()
+        except IOError as e:
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        except libvirt.libvirtError as e:
+            print "Libvirt Error({0}): {1}".format(e.errno, e.strerror)
+
+    # TODO: refactoring
+    def upload(self, volume, filename, offset=0, length=0, flags=0):
+        if not isinstance(volume, basestring):
+            raise StorageVolumeManagerError("'volume' field '{0}' is not valid".format(volume))
+
+        name = volume
+        volume = self.lookup(volume)
+        if not volume:
+            raise StorageVolumeManagerError("Unable to find volume '{0}'".format(name))
+        
+        try:
+            file = open(filename, 'rb')
+            stream = self._drv.newStream(flags=0)
+            # extra flags; not used yet, so callers should always pass 0
+            mime = volume.upload(stream, offset, length, flags=0)
+            stream.sendAll(lambda stream, data, fdes: fdes.write(data), file)
+            stream.finish()
+            file.close()
+        except IOError as e:
+            raise StorageVolumeManagerError("I/O error({0}): {1}".format(e.errno, e.strerror))
+        except libvirt.libvirtError as e:
+            raise StorageVolumeManagerError("Libvirt Error({0}): {1}".format(e.errno, e.strerror))
