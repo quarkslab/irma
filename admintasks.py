a0c0e69d94f7e084b7067f8a69967937803ebbb2
aquint <aquint@quarkslab.com>
2014-01-21 16:47:27 +0100

    update admintasks

--- admintasks.py
+++ admintasks.py
@@ -1,59 +1,58 @@
 from config.adminconfig import admin_celery
-from lib.irma.database.objects import IrmaConfig
 from lib.irma.common.utils import IrmaTaskReturn
+from lib.irma.common.objects import System
+from lib.irma.common.exceptions import IrmaAdminError
 
-# ______________________________________________________________ GLOBAL TASKS
 
-@admin_celery.task()
-def shutdown():
-    return IrmaTaskReturn.success("To be done")
-
-@admin_celery.task()
-def start():
-    return IrmaTaskReturn.success("To be done")
-
+system = System(nodes_cs=['qemu+tcp://192.168.130.1/system'])
 
 # ______________________________________________________________ NODE TASKS
 
 @admin_celery.task()
-def get_all_nodes():
-    return IrmaTaskReturn.success("To be done")
-
-@admin_celery.task()
-def add_node():
-    return IrmaTaskReturn.success("To be done")
-
-@admin_celery.task()
-def del_node():
-    return IrmaTaskReturn.success("To be done")
-
-@admin_celery.task()
-def get_node_by_probe_type():
-    return IrmaTaskReturn.success("To be done")
+def list_nodes():
+    try:
+        return IrmaTaskReturn.success(system.list_nodes())
+    except Exception, e:
+        return IrmaTaskReturn.error(str(e))
 
 # ______________________________________________________________ PROBE TASKS
 
 @admin_celery.task()
-def add_probe():
-    return IrmaTaskReturn.success("To be done")
+def probe_list():
+    try:
+        return IrmaTaskReturn.success(system.probe_list())
+    except Exception, e:
+        return IrmaTaskReturn.error(str(e))
+
+@admin_celery.task()
+def probe_master_list():
+    try:
+        return IrmaTaskReturn.success(system.probe_master_list())
+    except Exception, e:
+        return IrmaTaskReturn.error(str(e))
+
+@admin_celery.task()
+def probe_clone(node, label, dstlabel):
+    try:
+        return IrmaTaskReturn.success(system.probe_clone(node, label, dstlabel))
+    except Exception, e:
+        return IrmaTaskReturn.error(str(e))
 
 @admin_celery.task()
 def del_probe():
     return IrmaTaskReturn.success("To be done")
 
 @admin_celery.task()
-def start_probe():
-    return IrmaTaskReturn.success("To be done")
+def probe_start(node, label):
+    try:
+        return IrmaTaskReturn.success(system.probe_start(node, label))
+    except Exception, e:
+        return IrmaTaskReturn.error(str(e))
 
 @admin_celery.task()
-def stop_probe():
-    return IrmaTaskReturn.success("To be done")
-
-@admin_celery.task()
-def delete_probe():
-    return IrmaTaskReturn.success("To be done")
-
-@admin_celery.task()
-def get_probe_by_type():
-    return IrmaTaskReturn.success("To be done")
+def probe_stop(node, label):
+    try:
+        return IrmaTaskReturn.success(system.probe_stop(node, label))
+    except Exception, e:
+        return IrmaTaskReturn.error(str(e))
 
