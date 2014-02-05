e3b14c960a10595b191130db86bee1753d04601f
Fernand Lone-Sang <flonesang@quarkslab.com>
2014-02-05 17:29:29 +0100

    fixed bugs in python-magic wrapper (mimetypes.py)

--- common/mimetypes.py
+++ common/mimetypes.py
@@ -9,19 +9,7 @@ class Magic(object):
     ##########################################################################
 
     @classmethod
-    def _load_database(cls, filepath=None):
-        # load database
-        if filepath:
-            if os.path.exists(filepath):
-                cls.cookie.load(filepath)
-            else:
-                log.warning("magic database {0} does not exist, reverting to default.".format(filepath))
-                cls.cookie.load()
-        else:
-            cls.cookie.load()
-
-    @classmethod
-    def _set_flags(cls, mime=False, mime_encoding=False, keep_going=False):
+    def _initialize(cls, magic_file=None, mime=False, mime_encoding=False, keep_going=False):
         # setting flags
         cls.flags = magic.MAGIC_NONE
         if mime:
@@ -31,7 +19,22 @@ class Magic(object):
         if keep_going:
             cls.flags |= MAGIC_CONTINUE
         # creating cookie
-        cls.cookie = magic.open(cls.flags)       
+        try:
+            cls.cookie = magic.open(cls.flags)
+            # load database
+            if magic_file:
+                if os.path.exists(magic_file):
+                    cls.cookie.load(magic_file)
+                else:
+                    print("warning")
+                    log.warning("magic database {0} does not exist, reverting to default.".format(magic_file))
+                    cls.cookie.load()
+            else:
+                cls.cookie.load()
+        except AttributeError as e:
+            cls.cookie = magic.Magic(mime=mime, magic_file=magic_file, mime_encoding=mime_encoding, keep_going=keep_going)
+            cls.cookie.file = cls.cookie.from_file
+            cls.cookie.buffer = cls.cookie.from_buffer
 
     ##########################################################################
     # public methods
@@ -39,16 +42,12 @@ class Magic(object):
 
     @classmethod
     def from_buffer(cls, buf, **kwargs):
-        cls._set_flags(**kwargs)
-        cls._load_database(**kwargs)
+        cls._initialize(**kwargs)
         # perform processing
         try:
             filetype = cls.cookie.buffer(data)
         except:
-            try:
-                filetype = magic.from_buffer(data)
-            except Exception:
-                filetype = None
+            filetype = None
         finally:
             try:
                 cls.cookie.close()
@@ -59,16 +58,12 @@ class Magic(object):
     @classmethod
     def from_file(cls, filename, **kwargs):
         # reconfigure class before processing
-        cls._set_flags(**kwargs)
-        cls._load_database(**kwargs)
+        cls._initialize(**kwargs)
         # perform processing
         try:
             filetype = cls.cookie.file(filename)
         except:
-            try:
-                filetype = magic.from_file(filename)
-            except:
-                filetype = None
+            filetype = None
         finally:
             try:
                 cls.cookie.close()
