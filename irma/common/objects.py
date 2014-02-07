0ea3077b791d5ea2f7d3eca52ca205b7c7c838d3
aquint <aquint@quarkslab.com>
2014-02-07 17:24:06 +0100

    remove the no-more common objects (moved to frontend)

--- irma/common/objects.py
+++ irma/common/objects.py
@@ -1,6 +1,3 @@
-from datetime import datetime
-import config.dbconfig as dbconfig
-from lib.irma.database.objects import DatabaseObject
 
 class AttributeDictionary(dict):
     """A dictionnary with object-like accessors"""
@@ -8,53 +5,4 @@ class AttributeDictionary(dict):
     __getattr__ = lambda obj, key: obj.get(key, None)
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
-
-class ScanStatus:
-    init = 10
-    launched = 11
-    finished = 20
-    cancelling = 30
-    cancelled = 31
-
-    label = {
-             init:"scan created",
-             launched:"scan launched",
-             finished:"scan finished",
-             cancelled:"scan cancelled"
-    }
-
-class ScanInfo(DatabaseObject):
-    _uri = dbconfig.MONGODB
-    _dbname = dbconfig.DB_NAME
-    _collection = dbconfig.COLL_SCANINFO
-
-    def __init__(self, dbname=None, _id=None):
-        if dbname:
-            self._dbname = dbname
-        self.user = None
-        self.date = datetime.now()
-        self.oids = {}
-        self.taskid = None
-        self.probelist = []
-        self.status = ScanStatus.init
-        super(ScanInfo, self).__init__(_id=_id)
-
-    def get_results(self):
-        res = {}
-        for (oid, name) in self.oids.items():
-            r = ScanResults(_id=oid)
-            res[name] = dict((k, v) for (k, v) in r.results.iteritems() if k in self.probelist)
-        return res
-
-class ScanResults(DatabaseObject):
-    _uri = dbconfig.MONGODB
-    _dbname = dbconfig.DB_NAME
-    _collection = dbconfig.COLL_SCANRES
-
-    def __init__(self, dbname=None, _id=None):
-        if dbname:
-            self._dbname = dbname
-        self.probelist = []
-        self.results = {}
-        super(ScanResults, self).__init__(_id=_id)
 
