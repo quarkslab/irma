f9c39fa2e993bb6fd224dbc79bada9040c566894
aquint <aquint@quarkslab.com>
2014-02-10 10:28:26 +0100

    remove useless classes

--- irma/configuration/config.py
+++ irma/configuration/config.py
@@ -1,23 +1,7 @@
 from lib.irma.common.objects import AttributeDictionary
-from ConfigParser import RawConfigParser
 
 class Configuration(AttributeDictionary):
     pass
 
 class ConfigurationSection(Configuration):
     pass
-
-class TemplatedParam(object):
-    func = None
-
-    def __init__(self, default=None):
-        self.default = default
-
-class TpInteger(TemplatedParam):
-    func = RawConfigParser.getint
-
-class TpBoolean(TemplatedParam):
-    func = RawConfigParser.getboolean
-
-class TpString(TemplatedParam):
-    func = RawConfigParser.get
