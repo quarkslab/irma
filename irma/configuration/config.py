cb7da54f6ecafcdce62331c969a4f627aba014b7
aquint <aquint@quarkslab.com>
2014-02-10 10:21:13 +0100

    add TemplatedConfiguration

--- irma/configuration/config.py
+++ irma/configuration/config.py
@@ -1,7 +1,23 @@
 from lib.irma.common.objects import AttributeDictionary
+from ConfigParser import RawConfigParser
 
-class Configuration(AttributeDictionary): 
+class Configuration(AttributeDictionary):
     pass
 
-class ConfigurationSection(Configuration): 
+class ConfigurationSection(Configuration):
     pass
+
+class TemplatedParam(object):
+    func = None
+
+    def __init__(self, default=None):
+        self.default = default
+
+class TpInteger(TemplatedParam):
+    func = RawConfigParser.getint
+
+class TpBoolean(TemplatedParam):
+    func = RawConfigParser.getboolean
+
+class TpString(TemplatedParam):
+    func = RawConfigParser.get
