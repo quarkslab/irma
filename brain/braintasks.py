d554d080fe57acfb4b0e1a1714a0c823bef791d0
aquint <aquint@quarkslab.com>
2014-02-10 10:52:39 +0100

    move brain config to Templatedconfig

--- brain/braintasks.py
+++ brain/braintasks.py
@@ -1,21 +1,28 @@
-from config.brainconfig import brain_celery
-import config.probeconfig as probeconfig
 from lib.irma.common.utils import IrmaTaskReturn
 from lib.irma.common.objects import ScanInfo, ScanResults, ScanStatus
 import uuid
 import time
+from celery import Celery
+from config import brain_config as conf
+import config
 
 # Time to cache the probe list
 # to avoid asking to rabbitmq
 PROBELIST_CACHE_TIME = 60
-
 cache_probelist = {'list':None, 'time':None}
+
+brain_celery = Celery('braintasks')
+config.conf_celery(brain_celery)
+config.conf_celery_queue(brain_celery, conf.broker_brain.queue)
+
+probe_celery = Celery('probetasks')
+config.conf_celery(probe_celery)
 
 def get_probelist():
     now = time.time()
     if not cache_probelist['list'] or (now - cache_probelist['time']) > PROBELIST_CACHE_TIME:
         slist = list()
-        i = probeconfig.probe_celery.control.inspect()
+        i = probe_celery.control.inspect()
         queues = i.active_queues()
         for infolist in queues.values():
             for info in infolist:
@@ -50,18 +57,18 @@ def scan(scanid, oids, probelist, force):
         for probe in scaninfo.probelist:
             if oid_info['new'] or force:
                 # create one subtask per oid to scan per antivirus queue
-                jobs_list.append(probeconfig.probe_celery.send_task("probe.probetasks.probe_scan", args=(scanid, oid), queue=probe))
+                jobs_list.append(probe_celery.send_task("probe.probetasks.probe_scan", args=(scanid, oid), queue=probe))
             else:
                 # TODO update new filename for known oid if different
                 # check if resuls for all probe is already there else launch specific scan
                 scanresult = ScanResults(_id=oid)
                 if probe not in scanresult.results.keys():
-                    jobs_list.append(probeconfig.probe_celery.send_task("probe.probetasks.probe_scan", args=(scanid, oid), queue=probe))
+                    jobs_list.append(probe_celery.send_task("probe.probetasks.probe_scan", args=(scanid, oid), queue=probe))
 
     if len(jobs_list) != 0:
         # Build a result set with all job AsyncResult for progress/cancel operations
         groupid = str(uuid.uuid4())
-        groupres = probeconfig.probe_celery.GroupResult(id=groupid, results=jobs_list)
+        groupres = probe_celery.GroupResult(id=groupid, results=jobs_list)
 
         # keep the groupresult object for task status/cancel
         groupres.save()
@@ -86,7 +93,7 @@ def scan_progress(scanid):
     elif s.status == ScanStatus.launched:
         if not s.taskid:
             return IrmaTaskReturn.error("task_id not set")
-        job = probeconfig.probe_celery.GroupResult.restore(s.taskid)
+        job = probe_celery.GroupResult.restore(s.taskid)
         if not job:
             return IrmaTaskReturn.error("not a valid taskid")
         else:
@@ -111,7 +118,7 @@ def scan_cancel(scanid):
     if s.status == ScanStatus.init:
         return IrmaTaskReturn.error("task not launched")
     elif s.status == ScanStatus.launched:
-        job = probeconfig.probe_celery.GroupResult.restore(s.taskid)
+        job = probe_celery.GroupResult.restore(s.taskid)
         if not job:
             return IrmaTaskReturn.error("not a valid taskid")
         else:
---
+++ config/__init__.py
@@ -0,0 +1,85 @@
+import os
+from kombu import Queue
+from lib.irma.configuration.ini import TemplatedConfiguration
+
+# ______________________________________________________________________________ TEMPLATE
+
+template_brain_config = {
+                         'mongodb': (('host', TemplatedConfiguration.string, None),
+                                      ('port', TemplatedConfiguration.integer, 27017),
+                                      ('dbname', TemplatedConfiguration.string, None)
+                                    ),
+                         'collections': (('scan_info', TemplatedConfiguration.string, None),
+                                          ('scan_results', TemplatedConfiguration.string, None),
+                                          ('scan_files', TemplatedConfiguration.string, None),
+                                          ('scan_file_fs', TemplatedConfiguration.string, None)
+                                         ),
+                         'broker_brain': (
+                                    ('host', TemplatedConfiguration.string, None),
+                                    ('port', TemplatedConfiguration.integer, 5671),
+                                    ('vhost', TemplatedConfiguration.string, None),
+                                    ('username', TemplatedConfiguration.string, None),
+                                    ('password' , TemplatedConfiguration.string, None),
+                                    ('queue' , TemplatedConfiguration.string, None)
+                                    ),
+                         'broker_probe': (
+                                    ('host', TemplatedConfiguration.string, None),
+                                    ('port', TemplatedConfiguration.integer, 5671),
+                                    ('vhost', TemplatedConfiguration.string, None),
+                                    ('username', TemplatedConfiguration.string, None),
+                                    ('password' , TemplatedConfiguration.string, None)
+                                    ),
+                         'backend_brain': (
+                                   ('host', TemplatedConfiguration.string, None),
+                                   ('port', TemplatedConfiguration.integer, 6379),
+                                   ('db', TemplatedConfiguration.integer, None),
+                                   ),
+                         'backend_probe': (
+                                   ('host', TemplatedConfiguration.string, None),
+                                   ('port', TemplatedConfiguration.integer, 6379),
+                                   ('db', TemplatedConfiguration.integer, None),
+                                   )
+                         }
+
+
+brain_config = TemplatedConfiguration("brain.ini", template_brain_config)
+
+# ______________________________________________________________________________ CELERY HELPERS
+
+def conf_celery(app):
+    app.conf.update(
+                    CELERY_ACCEPT_CONTENT=['json'],
+                    CELERY_TASK_SERIALIZER='json',
+                    CELERY_RESULT_SERIALIZER='json'
+    )
+
+def conf_celery_queue(app, queue):
+    app.conf.update(
+                    CELERY_DEFAULT_QUEUE=queue,
+                    # delivery_mode=1 enable transient mode (don't survive to a server restart)
+                    CELERY_QUEUES=(
+                                   Queue(queue, routing_key=queue, delivery_mode=1),
+                                   )
+                    )
+
+# ______________________________________________________________________________ BACKEND HELPERS
+
+def _get_backend_uri(brain_config):
+    return "redis://%s:%s/%s" % (brain_config.host, brain_config.port, brain_config.db)
+
+def get_brain_backend_uri():
+    return _get_backend_uri(brain_config.backend_brain)
+
+def get_probe_backend_uri():
+    return _get_backend_uri(brain_config.backend_probe)
+
+# ______________________________________________________________________________ BROKER HELPERS
+
+def _get_broker_uri(broker_config):
+    return  "amqp://%s:%s@%s:%s/%s" % (broker_config.username, broker_config.password, broker_config.host, broker_config.port, broker_config.vhost)
+
+def get_brain_broker_uri():
+    return _get_broker_uri(brain_config.broker_brain)
+
+def get_probe_broker_uri():
+    return _get_broker_uri(brain_config.broker_probe)
