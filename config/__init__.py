e4cf486cf1acd1531f1c46eee1c1e73ec2989ef6
aquint <aquint@quarkslab.com>
2014-02-10 14:51:22 +0100

    move to .ini config file

---
+++ config/__init__.py
@@ -0,0 +1,89 @@
+import os
+from kombu import Queue
+from lib.irma.configuration.ini import TemplatedConfiguration
+
+# ______________________________________________________________________________ TEMPLATE
+
+template_frontend_config = {
+                         'mongodb': (('host', TemplatedConfiguration.string, None),
+                                      ('port', TemplatedConfiguration.integer, 27017),
+                                      ('dbname', TemplatedConfiguration.string, None)
+                                    ),
+                         'collections': (('scan_info', TemplatedConfiguration.string, None),
+                                          ('scan_results', TemplatedConfiguration.string, None),
+                                          ('scan_files', TemplatedConfiguration.string, None),
+                                          ('scan_file_fs', TemplatedConfiguration.string, None)
+                                         ),
+                         'broker_brain': (
+                                    ('host', TemplatedConfiguration.string, None),
+                                    ('port', TemplatedConfiguration.integer, 5671),
+                                    ('vhost', TemplatedConfiguration.string, None),
+                                    ('username', TemplatedConfiguration.string, None),
+                                    ('password' , TemplatedConfiguration.string, None),
+                                    ('queue' , TemplatedConfiguration.string, None)
+                                    ),
+                         'broker_admin': (
+                                    ('host', TemplatedConfiguration.string, None),
+                                    ('port', TemplatedConfiguration.integer, 5671),
+                                    ('vhost', TemplatedConfiguration.string, None),
+                                    ('username', TemplatedConfiguration.string, None),
+                                    ('password' , TemplatedConfiguration.string, None)
+                                    ('queue' , TemplatedConfiguration.string, None)
+                                    ),
+                         'backend_brain': (
+                                   ('host', TemplatedConfiguration.string, None),
+                                   ('port', TemplatedConfiguration.integer, 6379),
+                                   ('db', TemplatedConfiguration.integer, None),
+                                   ),
+                         'backend_admin': (
+                                   ('host', TemplatedConfiguration.string, None),
+                                   ('port', TemplatedConfiguration.integer, 6379),
+                                   ('db', TemplatedConfiguration.integer, None),
+                                   )
+                         }
+
+
+frontend_config = TemplatedConfiguration("frontend.ini", template_frontend_config)
+
+# ______________________________________________________________________________ CELERY HELPERS
+
+def conf_celery(app):
+    app.conf.update(
+                    CELERY_ACCEPT_CONTENT=['json'],
+                    CELERY_TASK_SERIALIZER='json',
+                    CELERY_RESULT_SERIALIZER='json'
+    )
+
+def conf_celery_queue(app, queue):
+    app.conf.update(
+                    CELERY_DEFAULT_QUEUE=queue,
+                    # delivery_mode=1 enable transient mode (don't survive to a server restart)
+                    CELERY_QUEUES=(
+                                   Queue(queue, routing_key=queue, delivery_mode=1),
+                                   )
+                    )
+
+def get_db_uri():
+    return "mongodb://%s:%s/" % (frontend_config.mongodb.host, frontend_config.mongodb.port)
+
+# ______________________________________________________________________________ BACKEND HELPERS
+
+def _get_backend_uri(backend_config):
+    return "redis://%s:%s/%s" % (backend_config.host, backend_config.port, backend_config.db)
+
+def get_brain_backend_uri():
+    return _get_backend_uri(frontend_config.backend_brain)
+
+def get_admin_backend_uri():
+    return _get_backend_uri(frontend_config.backend_admin)
+
+# ______________________________________________________________________________ BROKER HELPERS
+
+def _get_broker_uri(broker_config):
+    return  "amqp://%s:%s@%s:%s/%s" % (broker_config.username, broker_config.password, broker_config.host, broker_config.port, broker_config.vhost)
+
+def get_brain_broker_uri():
+    return _get_broker_uri(frontend_config.broker_brain)
+
+def get_probe_broker_uri():
+    return _get_broker_uri(frontend_config.broker_admin)
---
+++ frontend/lib/objects.py
@@ -0,0 +1,57 @@
+from lib.irma.database.objects import DatabaseObject
+import config
+from datetime import datetime
+
+cfg_dburi = config.get_db_uri()
+cfg_dbname = config.frontend_config.mongodb.db_name
+cfg_coll = config.frontend_config.collections
+
+class ScanStatus:
+    init = 10
+    launched = 11
+    finished = 20
+    cancelling = 30
+    cancelled = 31
+
+    label = {
+             init:"scan created",
+             launched:"scan launched",
+             finished:"scan finished",
+             cancelled:"scan cancelled"
+    }
+
+class ScanInfo(DatabaseObject):
+    _uri = cfg_dburi
+    _dbname = cfg_dbname
+    _collection = cfg_coll.scan_info
+
+    def __init__(self, dbname=None, _id=None):
+        if dbname:
+            self._dbname = dbname
+        self.user = None
+        self.date = datetime.now()
+        self.oids = {}
+        self.taskid = None
+        self.probelist = []
+        self.status = ScanStatus.init
+        super(ScanInfo, self).__init__(_id=_id)
+
+    def get_results(self):
+        res = {}
+        for (oid, name) in self.oids.items():
+            r = ScanResults(_id=oid)
+            res[name] = dict((k, v) for (k, v) in r.results.iteritems() if k in self.probelist)
+        return res
+
+class ScanResults(DatabaseObject):
+    _uri = cfg_dburi
+    _dbname = cfg_dbname
+    _collection = cfg_coll.scan_res
+
+    def __init__(self, dbname=None, _id=None):
+        if dbname:
+            self._dbname = dbname
+        self.probelist = []
+        self.results = {}
+        super(ScanResults, self).__init__(_id=_id)
+
