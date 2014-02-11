c3d9422a815762dad05b9659f26dace7edd0d110
aquint <aquint@quarkslab.com>
2014-02-11 15:19:35 +0100

    add setuptools package script

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author='QuarksLab',
     author_email='irma@quarkslab.com',
     packages=find_packages(),
+    include_package_data=True,
     license='LICENSE',
     description='Brain package for IRMA',
     long_description=open('README.rst').read(),
