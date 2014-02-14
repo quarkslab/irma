1aeb315eccf340c2c11ac2bc9470430d70ad5aa1
aquint <aquint@quarkslab.com>
2014-02-14 10:43:04 +0100

    add ftptls class and unittest

---
+++ irma/ftp/handler.py
@@ -0,0 +1,139 @@
+import logging
+from hashlib import sha1
+from irma.common.exceptions import IrmaFtpError
+from ftplib import FTP_TLS
+import os
+
+log = logging.getLogger(__name__)
+
+class FtpTls(object):
+    """Internal database.
+
+    This class handles the conection with ftp server over tls
+    functions for interacting with it.
+    """
+
+    ##########################################################################
+    # Constructor and Destructor stuff
+    ##########################################################################
+    def __init__(self, host, port, user, passwd):
+        self._host = host
+        self._port = port
+        self._user = user
+        self._passwd = passwd
+        self._conn = None
+        self._connect()
+
+    def __del__(self):
+        if self._conn:
+            self._disconnect()
+
+    def __enter__(self):
+        return self
+
+
+    ##########################################################################
+    # Private methods
+    ##########################################################################
+
+    def _connect(self):
+        if self._conn:
+            logging.warn("Already connected to ftp server")
+        try:
+            self._conn = FTP_TLS(self._host, self._user, self._passwd)
+            # Explicitely ask for secure data channel
+            self._conn.prot_p()
+        except Exception as e:
+            raise IrmaFtpError("{0}".format(e))
+
+    def _disconnect(self):
+        if not self._conn:
+            return
+        try:
+            self._conn.close()
+        except Exception as e:
+            raise IrmaFtpError("{0}".format(e))
+
+    def _hash(self, filename):
+        try:
+            with open(filename, "rb") as f:
+                data = f.read()
+            return sha1(data).hexdigest()
+        except Exception as e:
+            raise IrmaFtpError("{0}".format(e))
+
+    def _check_hash(self, digest, filename):
+        try:
+            with open(filename, "rb") as f:
+                if sha1(f.read()).hexdigest != digest:
+                    raise IrmaFtpError("Integrity check file failed")
+        except Exception as e:
+            raise IrmaFtpError("{0}".format(e))
+
+    ##########################################################################
+    # Public methods
+    ##########################################################################
+    def mkdir(self, path):
+        try:
+            self._conn.mkd(path)
+        except Exception as e:
+            raise IrmaFtpError("{0}".format(e))
+        return
+
+    def list(self, path):
+        """ list remote directory <path>"""
+        try:
+            return self._conn.nlst(path)
+        except Exception as e:
+            raise IrmaFtpError("{0}".format(e))
+
+    def upload(self, path, filename):
+        """ Upload <data> to remote <filename> into directory <path>"""
+        try:
+            dstname = self._hash(filename)
+            with open(filename, "rb") as f:
+                self._conn.storbinary("STOR {0}/{1}".format(path, dstname), f)
+            return dstname
+        except Exception as e:
+            raise IrmaFtpError("{0}".format(e))
+
+    def download(self, path, remotename, dstname):
+        """ Download <data> to remote <filename> into directory <path>"""
+        try:
+            with open(dstname, 'wb') as f:
+                self._conn.retrbinary("RETR {0}/{1}".format(path, remotename), lambda data: f.write(data))
+            # as remotename is the digest value of the file
+            self._check_hash(remotename, dstname)
+        except Exception as e:
+            raise IrmaFtpError("{0}".format(e))
+
+    def delete(self, path, filename):
+        """ Delete <filename> into directory <path>"""
+        try:
+            self._conn.delete("{0}/{1}".format(path, filename))
+        except Exception as e:
+            raise IrmaFtpError("{0}".format(e))
+
+    def deletepath(self, path, deleteParent=False):
+        # recursively delete all subdirs and files
+        try:
+            for f in self.list(path):
+                if self.is_file(path, f):
+                    self.delete(path, f)
+                else:
+                    self.deletepath("{0}/{1}".format(path, f), deleteParent=True)
+            if deleteParent:
+                self._conn.rmd(path)
+        except Exception as e:
+            raise IrmaFtpError("{0} [{1}]".format(e, path))
+
+    def is_file(self, path, filename):
+        try:
+            pathfilename = "{0}/{1}".format(path, filename)
+            current = self._conn.pwd()
+            self._conn.cwd(pathfilename)
+            self._conn.cwd(current)
+            return False
+        except:
+            return True
+        raise IrmaFtpError("Unable to test file {0}/{1}".format(path, filename))
---
+++ tests/test_ftp.py
@@ -0,0 +1,131 @@
+import hashlib, unittest, tempfile
+from irma.ftp.handler import FtpTls
+from irma.common.exceptions import IrmaFtpError
+
+##############################################################################
+# Test Cases
+##############################################################################
+class FtpTestCase(unittest.TestCase):
+    # Test config
+    test_ftp_host = "localhost"
+    test_ftp_port = "21"
+    test_ftp_user = "testuser"
+    test_ftp_passwd = "testpasswd"
+
+    def setUp(self):
+        # check database is ready for test
+        self.ftp_running()
+        self.flush_all()
+
+    def tearDown(self):
+        # do the teardown
+        self.flush_all()
+
+    def flush_all(self):
+        # check that ftp server is empty before running tests
+        try:
+            ftps = FtpTls(self.test_ftp_host, self.test_ftp_port, self.test_ftp_user, self.test_ftp_passwd)
+            ftps.deletepath("/")
+        except IrmaFtpError as e:
+            print "Testftp Error: {0}".format(e)
+            self.skipTest(FtpTestCase)
+
+    def ftp_running(self):
+        # check that ftp is up before running tests
+        try:
+            FtpTls(self.test_ftp_host, self.test_ftp_port, self.test_ftp_user, self.test_ftp_passwd)
+        except IrmaFtpError as e:
+            print "Testftp Error: {0}".format(e)
+            self.skipTest(FtpTestCase)
+
+
+
+class CheckFtpHandler(FtpTestCase):
+
+    def test_ftp_upload_file(self):
+        ftps = FtpTls(self.test_ftp_host, self.test_ftp_port, self.test_ftp_user, self.test_ftp_passwd)
+        hashname = ftps.upload("/", "test.ini")
+        self.assertEquals(len(ftps.list("/")), 1)
+        self.assertEquals(hashname , hashlib.sha1(open("test.ini").read()).hexdigest())
+
+    def test_ftp_create_dir(self):
+        ftps = FtpTls(self.test_ftp_host, self.test_ftp_port, self.test_ftp_user, self.test_ftp_passwd)
+        ftps.mkdir("test1")
+        self.assertEquals(len(ftps.list("/")), 1)
+
+    def test_ftp_create_subdir(self):
+        ftps = FtpTls(self.test_ftp_host, self.test_ftp_port, self.test_ftp_user, self.test_ftp_passwd)
+        ftps.mkdir("/test1")
+        ftps.mkdir("/test1/test2")
+        ftps.mkdir("/test1/test2/test3")
+        self.assertEquals(len(ftps.list("/")), 1)
+        self.assertEquals(len(ftps.list("/test1")), 1)
+        self.assertEquals(len(ftps.list("/test1/test2")), 1)
+        self.assertEquals(len(ftps.list("/test1/test2/test3")), 0)
+
+    def test_ftp_upload_in_subdir(self):
+        ftps = FtpTls(self.test_ftp_host, self.test_ftp_port, self.test_ftp_user, self.test_ftp_passwd)
+        ftps.mkdir("/test1")
+        ftps.mkdir("/test1/test2")
+        ftps.mkdir("/test1/test2/test3")
+        hashname = ftps.upload("/test1/test2/test3", "test.ini")
+        self.assertEquals(len(ftps.list("/test1/test2/test3")), 1)
+        self.assertEquals(hashname , hashlib.sha1(open("test.ini").read()).hexdigest())
+
+    def test_ftp_remove_not_existing_file(self):
+        ftps = FtpTls(self.test_ftp_host, self.test_ftp_port, self.test_ftp_user, self.test_ftp_passwd)
+        with self.assertRaises(IrmaFtpError):
+            ftps.delete(".", "lkzndlkaznd")
+
+    def test_ftp_remove_not_existing_dir(self):
+        ftps = FtpTls(self.test_ftp_host, self.test_ftp_port, self.test_ftp_user, self.test_ftp_passwd)
+        with self.assertRaises(IrmaFtpError):
+            ftps.deletepath("/test1", deleteParent=True)
+
+    def test_ftp_modify_file_hash(self):
+        ftps = FtpTls(self.test_ftp_host, self.test_ftp_port, self.test_ftp_user, self.test_ftp_passwd)
+        hashname = ftps.upload("/", "test.ini")
+        altered_name = "0000" + hashname[4:]
+        ftps._conn.rename(hashname, altered_name)
+        self.assertEquals(len(hashname), len(altered_name))
+        _, tmpname = tempfile.mkstemp(prefix="test_ftp")
+        with self.assertRaises(IrmaFtpError):
+            ftps.download(".", altered_name, tmpname)
+"""
+    def test_add_same_file(self):
+        db = Database(dbconfig.DBTEST_NAME, dbconfig.MONGODB)
+        dbh = db.db_instance()
+
+        database = dbh[dbconfig.DBTEST_NAME]
+        collection = database[dbconfig.COLL_FS + ".files"]
+        collection.remove()
+        collection = database[dbconfig.COLL_FS + ".chunks"]
+        collection.remove()
+        collection = database[dbconfig.COLL_FS + ".files"]
+
+        self.assertEquals(collection.count(), 0)
+        data = "TEST TEST TEST TEST"
+        f = FileObject(dbname=dbconfig.DBTEST_NAME)
+        self.assertEquals(f.save(data, "test.txt") , True)
+        self.assertEquals(collection.count(), 1)
+        self.assertEquals(f.altnames, [])
+
+        g = FileObject(dbname=dbconfig.DBTEST_NAME)
+        self.assertEquals(g.save(data, "test2.txt"), False)
+        self.assertEquals(collection.count(), 1)
+
+        self.assertEquals(g.hashvalue , hashlib.sha256(data).hexdigest())
+        self.assertEquals(g.name, "test.txt")
+        self.assertEquals(g.altnames, ["test2.txt"])
+        self.assertEquals(g.length, len(data))
+        self.assertIsNotNone(g._id)
+        self.assertEquals(f._id, g._id)
+
+        h = FileObject(dbname=dbconfig.DBTEST_NAME, _id=f._id)
+        self.assertEquals(h.name, "test.txt")
+        self.assertEquals(h.altnames, ["test2.txt"])
+"""
+
+if __name__ == '__main__':
+    unittest.main()
+
