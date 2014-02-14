888efd56c31ed78c232dd6c063d9a2c59f24c18b
aquint <aquint@quarkslab.com>
2014-02-14 10:48:34 +0100

    add launch_frontend server script

---
+++ scripts/launch_server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IRMA_DIR=/home/irma/irma
+PIDFILE=$IRMA_DIR/frontend.pid
+
+function launch_frontend {
+	cd $IRMA_DIR
+	python -m frontend.web.server 2> $IRMA_DIR/frontend.log &
+	echo -n $! > $PIDFILE
+}
+
+if [ -f $PIDFILE ] ; then
+	PID=`cat $PIDFILE`
+   	if ! ps -p $PID &> /dev/null; then
+	   launch_frontend
+	fi
+else
+	launch_frontend
+fi
