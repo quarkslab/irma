258a5fe4dfeda7296b47eae7ca51fbcdacde9971
aquint <aquint@quarkslab.com>
2014-02-14 16:27:06 +0100

    add common files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='irma-probe',
+    version='1.0',
+    author='QuarksLab',
+    author_email='irma@quarkslab.com',
+    packages=find_packages(),
+    include_package_data=True,
+    license='LICENSE',
+    description='Probe package for IRMA',
+    long_description=open('README.rst').read(),
+    install_requires = ['celery>=3.1.5','redis>=2.8.0','pymongo>=2.6.3','bottle>=0.11.6'],
+)
