dc3119e3b732730004e6fbd88bd9e2cbaca032d7
aquint <aquint@quarkslab.com>
2014-02-25 16:39:40 +0100

    fix startup script

--- scripts/celery.bat
+++ scripts/celery.bat
@@ -1,5 +1,5 @@
 SET WORKDIR=c:\irma
 
 SET LOGFILE=%WORKDIR%\celery.log
-c:\Python27\Scripts\celery -A probe.probetasks worker -l info -n avname --without-heartbeat --without-mingle --without-gossip --workdir=%WORKDIR% > %LOGFILE% 2>&1
+c:\Python27\Scripts\celery -A probe.tasks worker -l info -n avname --without-heartbeat --without-mingle --without-gossip --workdir=%WORKDIR% > %LOGFILE% 2>&1
 PAUSE
