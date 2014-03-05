78f3305b8867b288b9eed7cce688a83e830fd48d
Fernand Lone-Sang <flonesang@quarkslab.com>
2014-03-05 18:06:35 +0100

    added hash helper

---
+++ common/hash.py
@@ -0,0 +1,25 @@
+import hashlib
+
+##############################################################################
+# helper / decorator
+##############################################################################
+
+def generic_sum(algorithm):
+    def hash_sum(filename):
+        handle = algorithm()
+        with open(filename,'rb') as f: 
+            for chunk in iter(lambda: f.read(128 * handle.block_size), b''): 
+                handle.update(chunk)
+        return handle.hexdigest()
+    return hash_sum
+
+##############################################################################
+# declare sums
+##############################################################################
+
+md5sum    = generic_sum(hashlib.md5)
+sha1sum   = generic_sum(hashlib.sha1)
+sha224sum = generic_sum(hashlib.sha224)
+sha256sum = generic_sum(hashlib.sha256)
+sha384sum = generic_sum(hashlib.sha384)
+sha512sum = generic_sum(hashlib.sha512) 
