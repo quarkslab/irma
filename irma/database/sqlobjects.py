c4fca7896bd2c97fd6185c1a6f9ba7b7a38bbd7e
aquint <aquint@quarkslab.com>
2014-03-19 16:28:32 +0100

    add boolean type and foreignkey to sqlalchemy wrapper

--- irma/database/sqlobjects.py
+++ irma/database/sqlobjects.py
@@ -3,9 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Column = sqlalchemy.Column
 Integer = sqlalchemy.Integer
+Boolean = sqlalchemy.Boolean
 String = sqlalchemy.String
 DateTime = sqlalchemy.DateTime
 Base = declarative_base()
+ForeignKey = sqlalchemy.ForeignKey
 
 class SQLDatabaseObject(object):
     pass
