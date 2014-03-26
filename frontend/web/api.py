448e368f8acf51905ae2077e475ff2ff2a359826
aquint <aquint@quarkslab.com>
2014-03-26 12:26:59 +0100

    prod mode

--- frontend/web/api.py
+++ frontend/web/api.py
@@ -15,7 +15,7 @@ from bottle import route, request, default_app, run
     irma dependencies are no more required.
 """
 
-DEBUG = True
+DEBUG = False
 if DEBUG:
     from mucore import IrmaFrontendWarning, IrmaFrontendError, IrmaFrontendReturn
     import mucore as core
