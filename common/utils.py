95f45a6b594514ace7fb8bba6b90a3f3dd4b9d0f
aquint <aquint@quarkslab.com>
2014-03-26 16:24:45 +0100

    add time helper

--- common/utils.py
+++ common/utils.py
@@ -45,3 +45,50 @@ class MAC(object):
     @classmethod
     def normalize(cls, val):
         return str(val)
+
+# author liudmil-mitev
+# source https://github.com/liudmil-mitev/experiments/blob/master/time/humanize_time.py
+
+INTERVALS = [1, 60, 3600, 86400, 604800, 2419200, 29030400]
+NAMES = [('second', 'seconds'),
+         ('minute', 'minutes'),
+         ('hour', 'hours'),
+         ('day', 'days'),
+         ('week', 'weeks'),
+         ('month', 'months'),
+         ('year', 'years')]
+
+def humanize_time(amount, units):
+    """
+    Divide `amount` in time periods.
+    Useful for making time intervals more human readable.
+
+    >>> humanize_time(173, 'hours')
+    [(1, 'week'), (5, 'hours')]
+    >>> humanize_time(17313, 'seconds')
+    [(4, 'hours'), (48, 'minutes'), (33, 'seconds')]
+    >>> humanize_time(90, 'weeks')
+    [(1, 'year'), (10, 'months'), (2, 'weeks')]
+    >>> humanize_time(42, 'months')
+    [(3, 'years'), (6, 'months')]
+    >>> humanize_time(500, 'days')
+    [(1, 'year'), (5, 'months'), (3, 'weeks'), (3, 'days')]
+    """
+    result = []
+
+    unit = map(lambda a: a[1], NAMES).index(units)
+    # Convert to seconds
+    amount = amount * INTERVALS[unit]
+
+    for i in range(len(NAMES) - 1, -1, -1):
+        a = amount / INTERVALS[i]
+        if a > 0:
+            result.append((a, NAMES[i][1 % a]))
+            amount -= a * INTERVALS[i]
+    return result
+
+def humanize_time_str(amount, units):
+    res = []
+    for (value, unit) in humanize_time(amount, units):
+        res.append("{0} {1}".format(value, unit))
+    return ", ".join(res)
