c2dcd7a2bb31042651f16926276e56686870f0a5
aquint <aquint@quarkslab.com>
2014-04-02 17:00:40 +0200

    pep8 fixes

--- brain/objects.py
+++ brain/objects.py
@@ -1,7 +1,9 @@
 from lib.irma.database.sqlhandler import SQLDatabase
-from lib.irma.database.sqlobjects import Base, Column, Integer, String, DateTime
+from lib.irma.database.sqlobjects import Base, Column, \
+    Integer, String, DateTime
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+
 
 class Scan(Base):
     __tablename__ = 'scans'
@@ -14,7 +16,14 @@ class Scan(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
 
     def __repr__(self):
-        return "Scan {0}:\t{1} file(s)\t status: '{2}'\ttaskid: '{3}'\tuser_id: {4}\n".format(self.scanid, self.nbfiles, self.label[self.status], self.taskid, self.user_id)
+        str_repr = (
+            "Scan {0}:".format(self.scanid) +
+            "\t{0} file(s)".format(self.nbfiles) +
+            "\t status: '{0}'".format(self.label[self.status]) +
+            "\ttaskid: '{0}'".format(self.taskid) +
+            "\tuser_id: {0}\n".format(self.user_id))
+        return str_repr
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -26,17 +35,27 @@ class User(Base):
     scan = relationship("Scan")
 
     def __repr__(self):
-        return "User {0}:\trmq_vhost: '{1}'\t ftpuser: '{2}'\tquota: '{3}'\n".format(self.name, self.rmqvhost, self.ftpuser, self.quota)
+        str_repr = (
+            "User {0}:".format(self.name) +
+            "\trmq_vhost: '{0}'".format(self.rmqvhost) +
+            "\t ftpuser: '{0}'".format(self.ftpuser) +
+            "\tquota: '{0}'\n".format(self.quota))
+        return str_repr
 
 
 if __name__ == "__main__":
     # create all dbs
     import config.parser as config
 
-    sql = SQLDatabase(config.brain_config['sql_brain'].engine + config.brain_config['sql_brain'].dbname)
-    Base.metadata.create_all(sql._db)
+    engine = config.brain_config['sql_brain'].engine
+    dbname = config.brain_config['sql_brain'].dbname
+    sql = SQLDatabase(engine + dbname)
+    metadata = Base.metadata
+    metadata.create_all(sql._db)
     # quota is in number of files (0 means disabled)
-    user = User(name="test1", rmqvhost="mqfrontend", ftpuser="frontend1", quota=0)
+    user = User(name="test1",
+                rmqvhost="mqfrontend",
+                ftpuser="frontend1",
+                quota=0)
     sql.add(user)
     sql.commit()
-
