370d091984282e75b8feae49b3826cab12c0a776
Fernand Lone-Sang <flonesang@quarkslab.com>
2014-04-10 17:50:57 +0200

    fixed some pylint errors/warnings

--- common/compat.py
+++ common/compat.py
@@ -1,9 +1,15 @@
+""" Helpers for python 2 and python 3 compatibility
+
+This file should be imported in all modules
+"""
+
 import time
 
 
 def timestamp():
     """ On some systems, time.time() returns a float
     instead of an int. This function always returns an int
+
     :rtype: int
     :return: the current timestamp
     """
