b2602a7736364949e31ba46bfd40b3e0cc0ac3f2
Fernand Lone-Sang <flonesang@quarkslab.com>
2014-05-06 16:03:34 +0200

    Uniformize config parser

---
+++ config/parser.py
@@ -0,0 +1,127 @@
+import os
+
+from kombu import Queue
+from logging import BASIC_FORMAT, Formatter
+from logging.handlers import SysLogHandler
+from celery.log import redirect_stdouts_to_logger
+from celery.signals import after_setup_task_logger, after_setup_logger
+from lib.irma.configuration.ini import TemplatedConfiguration
+
+
+# ==========
+#  TEMPLATE
+# ==========
+
+template_probe_config = {
+    'log': [
+        ('syslog', TemplatedConfiguration.integer, 0),
+        ('prefix', TemplatedConfiguration.string, "irma-probe :"),
+    ],
+    'broker_probe': [
+        ('host', TemplatedConfiguration.string, None),
+        ('port', TemplatedConfiguration.integer, 5672),
+        ('vhost', TemplatedConfiguration.string, None),
+        ('username', TemplatedConfiguration.string, None),
+        ('password', TemplatedConfiguration.string, None)
+    ],
+    'backend_probe': [
+        ('host', TemplatedConfiguration.string, None),
+        ('port', TemplatedConfiguration.integer, 6379),
+        ('db', TemplatedConfiguration.integer, None),
+    ],
+    'ftp_brain': [
+        ('host', TemplatedConfiguration.string, None),
+        ('port', TemplatedConfiguration.integer, 21),
+        ('username', TemplatedConfiguration.string, None),
+        ('password', TemplatedConfiguration.string, None),
+    ],
+}
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+cfg_file = "{0}/{1}".format(cwd, "probe.ini")
+probe_config = TemplatedConfiguration(cfg_file, template_probe_config)
+
+# ================
+#  Celery helpers
+# ================
+
+def _conf_celery(app, broker, backend=None, queue=None):
+    app.conf.update(BROKER_URL=broker,
+                    CELERY_RESULT_BACKEND=backend,
+                    CELERY_ACCEPT_CONTENT=['json'],
+                    CELERY_TASK_SERIALIZER='json',
+                    CELERY_RESULT_SERIALIZER='json'
+                    )
+    if backend is not None:
+        app.conf.update(CELERY_RESULT_BACKEND=backend)
+    if queue is not None:
+        app.conf.update(CELERY_DEFAULT_QUEUE=queue,
+                        # delivery_mode=1 enable transient mode
+                        # (don't survive to a server restart)
+                        CELERY_QUEUES=(Queue(queue, routing_key=queue),)
+                        )
+    return
+
+
+def conf_probe_celery(app):
+    broker = get_probe_broker_uri()
+    backend = get_probe_backend_uri()
+    _conf_celery(app, broker, backend=backend)
+
+
+# =================
+#  Backend helpers
+# =================
+
+def _get_backend_uri(backend_config):
+    host = backend_config.host
+    port = backend_config.port
+    db = backend_config.db
+    return "redis://{host}:{port}/{db}" \
+           "".format(host=host, port=port, db=db)
+
+
+def get_probe_backend_uri():
+    return _get_backend_uri(probe_config.backend_probe)
+
+
+# ================
+#  Broker helpers
+# ================
+
+def _get_broker_uri(broker_config):
+    user = broker_config.username
+    pwd = broker_config.password
+    host = broker_config.host
+    port = broker_config.port
+    vhost = broker_config.vhost
+    return "amqp://{user}:{pwd}@{host}:{port}/{vhost}" \
+           "".format(user=user, pwd=pwd, host=host, port=port, vhost=vhost)
+
+
+def get_probe_broker_uri():
+    return _get_broker_uri(probe_config.broker_probe)
+
+
+# ================
+#  Syslog helpers
+# ================
+
+def configure_syslog(app):
+    if probe_config.log.syslog:
+        app.conf.update(CELERYD_LOG_COLOR=False)
+        after_setup_logger.connect(setup_log)
+        after_setup_task_logger.connect(setup_log)
+
+def setup_log(**args):
+    # redirect stdout and stderr to logger
+    redirect_stdouts_to_logger(args['logger'])
+    # logs to local syslog
+    hl = SysLogHandler('/dev/log', facility=SysLogHandler.facility_names['syslog'])
+    # setting log level
+    hl.setLevel(args['loglevel'])
+    # setting log format
+    formatter = Formatter(probe_config.log.prefix + BASIC_FORMAT)
+    hl.setFormatter(formatter)
+    # add new handler to logger
+    args['logger'].addHandler(hl)
