04d78406592ef40a755f616365a6ac1222ec0674
aquint <aquint@quarkslab.com>
2014-05-09 10:53:29 +0200

    bump version to 1.0.1 remove renamed install files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='irma-brain',
-    version='1.0',
+    version='1.0.1',
     author='QuarksLab',
     author_email='irma@quarkslab.com',
     packages=find_packages(),
@@ -10,5 +10,5 @@ setup(
     license='LICENSE',
     description='Brain package for IRMA',
     long_description=open('README.rst').read(),
-    scripts=['scripts/status.py'],
+    scripts=['scripts/status.py', 'scripts/update.sh'],
 )
