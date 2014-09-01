ff7d10ce7e2317b797828d9279349a7e5bbf960d
aquint <aquint@quarkslab.com>
2014-09-01 17:08:16 +0200

    add missing formatters

---
+++ frontend/formatters/antivirus/default.py
@@ -0,0 +1,48 @@
+#
+# Copyright (c) 2013-2014 QuarksLab.
+# This file is part of IRMA project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the top-level directory
+# of this distribution and at:
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# No part of the project, including this file, may be copied,
+# modified, propagated, or distributed except according to the
+# terms contained in the LICENSE file.
+
+from lib.plugins import PluginBase
+
+
+class AntivirusFormatterPlugin(PluginBase):
+
+    # =================
+    #  plugin metadata
+    # =================
+
+    _plugin_name_ = "AntivirusDefault"
+    _plugin_author_ = "IRMA (c) Quarkslab"
+    _plugin_version_ = "1.0.0"
+    _plugin_category_ = "antivirus"
+    _plugin_description_ = "Default Formatter for Antivirus category"
+    _plugin_dependencies_ = []
+
+    # ===========
+    #  Formatter
+    # ===========
+
+    @staticmethod
+    def can_handle_results(raw_result):
+        # New probe result format (from version 1.0.4)
+        return raw_result.get('type', None) == "antivirus"
+
+    @staticmethod
+    def format(raw_result):
+        # New probe result format (from version 1.0.4)
+        # As the raw_result has almost the same structure as the json for the
+        # output, we simply delete antivirus specific fields and we return the
+        # object
+        raw_result.pop('database', None)
+        return raw_result
