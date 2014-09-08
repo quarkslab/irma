6e3ebfbe9e28e6393b4d100d0dc29febab5b8a87
aquint <aquint@quarkslab.com>
2014-09-08 16:48:49 +0200

    scan_request is a dict now

--- brain/tasks.py
+++ brain/tasks.py
@@ -209,7 +209,7 @@ def scan(scanid, scan_request):
         scan.user_id = user.id
         sql.commit()
 
-        for (filename, probelist) in scan_request:
+        for (filename, probelist) in scan_request.items():
             if probelist is None:
                 scan.status = IrmaScanStatus.error_probe_missing
                 sql.commit()
@@ -225,7 +225,8 @@ def scan(scanid, scan_request):
                     log.debug("{0}: Unknown probe {1}".format(scanid, p))
                     return IrmaTaskReturn.error(msg)
 
-            # Now, create one subtask per file to scan per probe according to quota
+            # Now, create one subtask per file to scan
+            # per probe according to quota
             for probe in probelist:
                 if quota is not None and quota <= 0:
                     break
@@ -233,7 +234,10 @@ def scan(scanid, scan_request):
                     quota -= 1
                 callback_signature = route(
                     results_app.signature("brain.tasks.scan_result",
-                                          (user.ftpuser, scanid, filename, probe)))
+                                          (user.ftpuser,
+                                           scanid,
+                                           filename,
+                                           probe)))
                 jobs_list.append(
                     probe_app.send_task("probe.tasks.probe_scan",
                                         args=(user.ftpuser, scanid, filename),
