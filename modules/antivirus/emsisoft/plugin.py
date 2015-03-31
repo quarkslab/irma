deb854f68ef12c5c795a1d919fabf61da4afe95c
Fernand Lone-Sang <flonesang@quarkslab.com>
2015-03-31 10:48:22 +0200

    Emsisoft Asquared (see commit logs for install details)
    - the tested copy of Emsisoft Asquared is available at the following URL:
      http://download1.emsisoft.com/a2cmd.zip
    - the default search path is %PROGRAMFILES%/Emsisoft/a2cmd/. The archive must
      be unzipped in this folder
    - to trigger an update, run 'a2cmd.exe /update' from the command line

---
+++ modules/antivirus/emsisoft/plugin.py
@@ -0,0 +1,59 @@
+#
+# Copyright (c) 2013-2014 QuarksLab.
+# This file is part of IRMA project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the top-level directory
+# of this distribution and at:
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# No part of the project, including this file, may be copied,
+# modified, propagated, or distributed except according to the
+# terms contained in the LICENSE file.
+
+import os
+
+from .asquared import ASquaredCmd
+from ..interface import AntivirusPluginInterface
+
+from lib.plugins import PluginBase, PluginLoadError
+from lib.plugins import PlatformDependency
+from lib.irma.common.utils import IrmaProbeType
+
+
+class ASquaredCmdPlugin(PluginBase, ASquaredCmd, AntivirusPluginInterface):
+
+    # =================
+    #  plugin metadata
+    # =================
+
+    _plugin_name_ = "ASquaredCmd"
+    _plugin_author_ = "IRMA (c) Quarkslab"
+    _plugin_version_ = "1.0.0"
+    _plugin_category_ = IrmaProbeType.antivirus
+    _plugin_description_ = "Plugin for ASquaredCmd on Windows"
+    _plugin_dependencies_ = [
+        PlatformDependency('win32')
+    ]
+
+    @classmethod
+    def verify(cls):
+        # create an instance
+        module = ASquaredCmd()
+        path = module.scan_path
+        del module
+        # perform checks
+        if not path or not os.path.exists(path):
+            raise PluginLoadError("{0}: verify() failed because "
+                                  "ASquaredCmd executable was not found."
+                                  "".format(cls.__name__))
+
+    # =============
+    #  constructor
+    # =============
+
+    def __init__(self):
+        # load default configuration file
+        self.module = ASquaredCmd()
