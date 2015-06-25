ad86b91f88ba9d754f22024a79b09bd3c7a3db44
Fernand Lone-Sang <flonesang@quarkslab.com>
2015-06-25 12:16:37 +0200

    [Fix] Update copyright info

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2013-2014 QuarksLab.
+# Copyright (c) 2013-2015 QuarksLab.
 # This file is part of IRMA project.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
