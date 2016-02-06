e480d12384612ad9f0b3dcd2fdf1d0f592c91db6
ch0k0bn <aquint@quarkslab.com>
2016-02-06 16:50:51 +0100

    fix unittest

--- tests/api/v1_1/controllers/test_search.py
+++ tests/api/v1_1/controllers/test_search.py
@@ -1,34 +1,34 @@
 from unittest import TestCase
 from mock import MagicMock, patch
 
-import frontend.api.v1_1.controllers.search as api_search
+import frontend.api.v1_1.controllers.files as api_files
 from bottle import FormsDict
 
 
 class TestApiSearch(TestCase):
 
     def setUp(self):
-        self.old_file_web_schema = api_search.file_web_schema
-        self.old_request = api_search.request
+        self.old_file_web_schema = api_files.file_web_schema
+        self.old_request = api_files.request
         self.file_web_schema = MagicMock()
         self.request = MagicMock()
         self.request.query = FormsDict()
         self.db = MagicMock()
-        api_search.file_web_schema = self.file_web_schema
-        api_search.request = self.request
+        api_files.file_web_schema = self.file_web_schema
+        api_files.request = self.request
 
     def tearDown(self):
-        api_search.file_web_schema = self.old_file_web_schema
-        api_search.request = self.old_request
+        api_files.file_web_schema = self.old_file_web_schema
+        api_files.request = self.old_request
         del self.file_web_schema
         del self.request
 
     def test001_files_raise_none_None(self):
         self.request.query['name'] = "whatever"
         self.request.query.hash = "something"
-        process_error = "frontend.api.v1_1.controllers.search.process_error"
+        process_error = "frontend.api.v1_1.controllers.files.process_error"
         with patch(process_error) as mock:
-            api_search.files("whatever")
+            api_files.list("whatever")
             self.assertTrue(mock.called)
             self.assertIsInstance(mock.call_args[0][0], ValueError)
             self.assertEqual(str(mock.call_args[0][0]),
@@ -36,9 +36,9 @@ class TestApiSearch(TestCase):
 
     def test002_files_raise_h_type_None(self):
         self.request.query.hash = "something"
-        process_error = "frontend.api.v1_1.controllers.search.process_error"
+        process_error = "frontend.api.v1_1.controllers.files.process_error"
         with patch(process_error) as mock:
-            api_search.files("whatever")
+            api_files.list("whatever")
             self.assertTrue(mock.called)
             self.assertIsInstance(mock.call_args[0][0], ValueError)
             self.assertEqual(str(mock.call_args[0][0]), "Hash not supported")
