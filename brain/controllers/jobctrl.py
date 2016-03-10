b1e7ad09531100dbabfea2d41186886748cecedf
ch0k0bn <aquint@quarkslab.com>
2016-03-10 11:15:39 +0100

    Add a debug option for Celery tasks
    - New config item under section log, key debug (default False)
    - if enabled set all logger to debug level

--- brain/controllers/jobctrl.py
+++ brain/controllers/jobctrl.py
@@ -13,10 +13,13 @@
 # modified, propagated, or distributed except according to the
 # terms contained in the LICENSE file.
 
+import logging
 from brain.controllers import scanctrl
 from brain.models.sqlobjects import Job
 from brain.helpers.sql import session_query, session_transaction
 from lib.common.compat import timestamp
+
+log = logging.getLogger(__name__)
 
 
 def new(scan_id, filename, probe, task_id):
@@ -24,10 +27,13 @@ def new(scan_id, filename, probe, task_id):
         j = Job(filename, probe, scan_id, task_id)
         j.save(session)
         session.commit()
+        log.debug("scanid %s: job %s for file %s probe %s task_id %s",
+                  scan_id, j.id, filename, probe, task_id)
         return j.id
 
 
 def _finish(job_id, status):
+    log.debug("job_id: %s status %s", job_id, status)
     with session_transaction() as session:
         job = Job.load(job_id, session)
         job.status = status
@@ -46,6 +52,7 @@ def error(job_id):
 
 
 def set_taskid(job_id, task_id):
+    log.debug("job_id: %s task_id %s", job_id, task_id)
     with session_transaction() as session:
         job = Job.load(job_id, session)
         job.task_id = task_id
